ac095267301771fd12df40bacfd78da672fcb061
peheje <peterhj@gmail.com>
2021-12-04 20:43:24 +0100

    initial commit

---
+++ 2/Program.fs
@@ -0,0 +1,35 @@
+﻿let logs x = printfn "%A" x; x
+let split (s: string) =
+    let ss = s.Split " "
+    (ss[0], int ss[1])
+
+// Part 1
+let add2 (a1, a2) (b1, b2) = a1 + b1, a2 + b2
+let getActions (s: string) =
+    match split s with
+    | "forward", d -> (d, 0)
+    | "down", d -> (0, d)
+    | "up", d -> (0, -d)
+let pos, depth =
+    System.IO.File.ReadAllLines "2.txt"
+    |> Seq.map getActions
+    |> Seq.fold add2 (0, 0)
+let result1 = pos * depth
+
+// Part 2
+type State = {pos: int; depth: int; aim: int}
+let add s1 s2 = {pos = s1.pos + s2.pos; depth = s1.depth + s2.depth; aim = s1.aim + s2.aim}
+let nextAction aim command =
+    match command with
+    | "forward", d -> {pos = d; depth = aim*d; aim = 0}
+    | "down", d -> {pos = 0; depth = 0; aim = d}
+    | "up", d -> {pos = 0; depth = 0; aim = -d}
+
+let xs =
+    System.IO.File.ReadAllLines "2.txt"
+    |> Seq.map split
+    |> Seq.fold (fun state command ->
+        add (nextAction state.aim command) state
+    ) {pos = 0;depth = 0; aim = 0}
+    |> logs
+let result2 = xs.depth * xs.pos
---
+++ 3/Program.fs
@@ -0,0 +1,16 @@
+﻿let logs x = printfn "%A" x; x
+let xs = System.IO.File.ReadAllLines "1.txt"
+let xxs = xs |> Seq.transpose
+let bin2dec s = System.Convert.ToInt32(s, 2)
+let countWhere a = Seq.filter a >> Seq.length
+
+let msb2dec c xxs =
+    xxs
+    |> Seq.map (countWhere (fun x -> x = c))
+    |> Seq.map (fun c -> if c > (Seq.length xs) / 2 then "0" else "1")
+    |> String.concat ""
+    |> bin2dec
+
+let gamma = xxs |> msb2dec '0'
+let epsilon = xxs |> msb2dec '1'
+let ans = gamma * epsilon
