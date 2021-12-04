9238e0461559c677224f52ac7023520e1bab5043
peheje <peterhj@gmail.com>
2021-12-04 21:44:03 +0100

    prepare for part 2

--- 3/Program.fs
+++ 3/Program.fs
@@ -1,9 +1,10 @@
 ﻿let logs x = printfn "%A" x; x
-let xs = System.IO.File.ReadAllLines "1.txt"
+let xs = System.IO.File.ReadAllLines "sample.txt"
 let xxs = xs |> Seq.transpose
 let bin2dec s = System.Convert.ToInt32(s, 2)
 let countWhere a = Seq.filter a >> Seq.length
 
+// Part 2
 let msb2dec c xxs =
     xxs
     |> Seq.map (countWhere (fun x -> x = c))
@@ -13,4 +14,4 @@ let msb2dec c xxs =
 
 let gamma = xxs |> msb2dec '0'
 let epsilon = xxs |> msb2dec '1'
-let ans = gamma * epsilon
+let power = gamma * epsilon
