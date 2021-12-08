25d807bbf03f678f7719acde08b41d9de97fb65d
peheje <peterhj@gmail.com>
2021-12-08 20:47:51 +0100

    code-golf-3

--- 3/Program.fs
+++ 3/Program.fs
@@ -1,11 +1,9 @@
 ﻿// Common
-let log x = printfn "%A" x
 let xs = System.IO.File.ReadAllLines "3.txt"
 let bin2dec s = System.Convert.ToInt32(s, 2)
 let countWhere a = Seq.filter a >> Seq.length
 
 // Part 1
-let xxs = xs |> Seq.transpose
 let msb2dec c xxs =
     xxs
     |> Seq.map (countWhere (fun x -> x = c))
@@ -13,27 +11,26 @@ let msb2dec c xxs =
     |> String.concat ""
     |> bin2dec
 
+let xxs = xs |> Seq.transpose
 let gamma = xxs |> msb2dec '0'
 let epsilon = xxs |> msb2dec '1'
-let power = gamma * epsilon |> log
+let power = gamma * epsilon |> printfn "%A"
 
 // Part 2
 let mostOfAtIndex i (xs: string array) =
-    let zeroes = xs |> Seq.map (fun x -> x[i]) |> countWhere (fun x -> x = '0')
-    if zeroes > Seq.length xs / 2 then '0' else '1'
+    if xs |> countWhere (fun x -> x[i] = '0') > Seq.length xs / 2 then '0' else '1'
 
 let leastOfAtIndex i xs =
     if xs |> mostOfAtIndex i = '0' then '1' else '0'
 
 let filterByCriteria criteria strings =
     let rec filter i (filtered: string array) =
-        match Seq.length filtered with
-        | 1 -> filtered
-        | _ ->
+        if Seq.length filtered = 1 then filtered
+        else
             let target = filtered |> criteria i
             filter (i + 1) (filtered |> Array.filter (fun x -> x[i] = target))
     (filter 0 strings)[0]
 
 let oxygen = xs |> filterByCriteria mostOfAtIndex |> bin2dec
 let co2 = xs |> filterByCriteria leastOfAtIndex |> bin2dec
-let lifeSupport = oxygen * co2 |> log
+let lifeSupport = oxygen * co2 |> printfn "%A"
