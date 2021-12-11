cf088d4ad45dd19429c18186e9b250657f6d6df3
Peter <peterhj@gmail.com>
2021-12-11 14:26:36 +0100

    it works but ugly

--- 4/Program.fs
+++ 4/Program.fs
@@ -1,13 +1,15 @@
 ﻿let log x = printfn "%A" x
+let logs x = printfn "%A" x; x
 let debugl x = System.IO.File.WriteAllLines("debug.txt", (x |> Seq.toArray))
-let json s = System.Text.Json.JsonSerializer.Serialize s
+let json s = System.Text.Json.JsonSerializer.Serialize(s, System.Text.Json.JsonSerializerOptions(WriteIndented = true))
 let debugs s = System.IO.File.AppendAllText("debug.json", json s); s
 let lines = System.IO.File.ReadAllLines "sample.txt"
 let split (c: char) (s: string) = s.Split c
 let replace (a: string) (b: string) (s: string) = s.Replace(a, b)
 let isWhitespace (s: string) = System.String.IsNullOrWhiteSpace s
+let countWhere a = Seq.filter a >> Seq.length
 
-type Cell = { value: int; marked: bool; col: int; row: int; board: int }
+type Cell = { value: int; mutable marked: bool; col: int; row: int; board: int }
 
 let calls = lines |> Seq.head |> split ',' |> Seq.map int
 
@@ -16,7 +18,7 @@ let parseBingoRow s =
     |> split ' '
     |> Seq.filter (not << isWhitespace)
 
-let boards = 
+let boardsList = 
     lines
     |> Seq.filter (not << Seq.isEmpty)
     |> Seq.skip 1
@@ -24,13 +26,29 @@ let boards =
     |> Seq.chunkBySize 5
 
 let cells = [
-    for (bi, board) in boards |> Seq.indexed do
+    for (bi, board) in boardsList |> Seq.indexed do
         for (ri, row) in board |> Seq.indexed do
             for (ci, value) in row |> Seq.indexed do
                 {value = int value; marked = false; col = ci; row = ri; board = bi}
 ]
 
-cells
-|> Seq.filter (fun cell -> cell.board = 0)
-|> Seq.filter (fun cell -> cell.col = 0)
-|> Seq.map (fun cell -> cell.value)
+let nRows = (cells |> Seq.map (fun c -> c.row) |> Seq.max) + 1
+let nCols = (cells |> Seq.map (fun c -> c.col) |> Seq.max) + 1
+
+let boards = cells |> Seq.groupBy (fun c -> c.board) |> Seq.map (fun t -> snd t)
+
+for call in calls do
+    for cell in cells do
+        if cell.value = call then cell.marked <- true
+
+    for board in boards do
+        let marked = board |> Seq.filter (fun c -> c.marked)
+        let bingoRow = marked |> Seq.groupBy (fun c -> c.row) |> Seq.exists (fun g -> ((snd g) |> Seq.length) = nCols)
+        let bingoCol = marked |> Seq.groupBy (fun c -> c.col) |> Seq.exists (fun g -> ((snd g) |> Seq.length) = nRows)
+
+        if bingoRow || bingoCol then
+            let unmarkedOnBoard = board |> Seq.filter (fun c -> not c.marked) |> Seq.sumBy (fun c -> c.value)
+            printfn $"bingo ${unmarkedOnBoard * call}"
+            failwith "end"
+
+
