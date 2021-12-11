0763e8e6be8f76aa605b7b4a54ecc3fea9dda093
Peter <peterhj@gmail.com>
2021-12-11 15:06:15 +0100

    simplify

--- 4/Program.fs
+++ 4/Program.fs
@@ -1,5 +1,5 @@
 ﻿let logs x = printfn "%A" x; x
-let json s = System.Text.Json.JsonSerializer.Serialize(s, System.Text.Json.JsonSerializerOptions(WriteIndented = true))
+let json s = System.Text.Json.JsonSerializer.Serialize s
 let debugs s = System.IO.File.AppendAllText("debug.json", json s); s
 let split (c: char) (s: string) = s.Split c
 let isWhitespace (s: string) = System.String.IsNullOrWhiteSpace s
@@ -7,7 +7,8 @@ let parseRow s = s |> split ' ' |> Seq.filter (not << isWhitespace)
 
 type Cell = { value: int; mutable marked: bool; col: int; row: int; board: int }
 
-let lines = System.IO.File.ReadAllLines "sample.txt"
+let size = 5
+let lines = System.IO.File.ReadAllLines "4.txt"
 let calls = lines |> Seq.head |> split ',' |> Seq.map int
 
 let boardsList = 
@@ -15,7 +16,7 @@ let boardsList =
     |> Seq.filter (not << Seq.isEmpty)
     |> Seq.skip 1
     |> Seq.map parseRow
-    |> Seq.chunkBySize 5
+    |> Seq.chunkBySize size
 
 let cells = [
     for (bi, board) in boardsList |> Seq.indexed do
@@ -23,9 +24,6 @@ let cells = [
             for (ci, value) in row |> Seq.indexed do
                 {value = int value; marked = false; col = ci; row = ri; board = bi}
 ]
-
-let nRows = (cells |> Seq.map (fun c -> c.row) |> Seq.max) + 1
-let nCols = (cells |> Seq.map (fun c -> c.col) |> Seq.max) + 1
 
 let boards = cells |> Seq.groupBy (fun c -> c.board) |> Seq.map (fun t -> snd t)
 
@@ -35,8 +33,8 @@ for call in calls do
 
     for board in boards do
         let marked = board |> Seq.filter (fun c -> c.marked)
-        let bingoRow = marked |> Seq.groupBy (fun c -> c.row) |> Seq.exists (fun g -> ((snd g) |> Seq.length) = nCols)
-        let bingoCol = marked |> Seq.groupBy (fun c -> c.col) |> Seq.exists (fun g -> ((snd g) |> Seq.length) = nRows)
+        let bingoRow = marked |> Seq.groupBy (fun c -> c.row) |> Seq.exists (fun g -> ((snd g) |> Seq.length) = size)
+        let bingoCol = marked |> Seq.groupBy (fun c -> c.col) |> Seq.exists (fun g -> ((snd g) |> Seq.length) = size)
 
         if bingoRow || bingoCol then
             let unmarkedOnBoard = board |> Seq.filter (fun c -> not c.marked) |> Seq.sumBy (fun c -> c.value)
