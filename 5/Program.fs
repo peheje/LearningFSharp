fe1ab328cc52225da7a44ff122731d2b880c217d
Peter <peterhj@gmail.com>
2021-12-12 15:58:55 +0100

    move common code into function

--- 5/Program.fs
+++ 5/Program.fs
@@ -14,25 +14,43 @@ let toLine s =
     let ss = s |> (split " -> ")
     {start = toCoordinate(ss[0]); stop = toCoordinate(ss[1])}
 
-let isHorizontalOrVertical line =
-    line.start.x = line.stop.x || line.start.y = line.stop.y
+let isHorisontal line = line.start.y = line.stop.y
+
+let isVertical line = line.start.x = line.stop.x
+
+let isHorizontalOrVertical line = isHorisontal line || isVertical line
 
 let trajectory line =
     let xstep = if line.start.x > line.stop.x then -1 else 1
     let ystep = if line.start.y > line.stop.y then -1 else 1
-    [|
-        for x in line.start.x .. xstep .. line.stop.x do
-            for y in line.start.y .. ystep ..line.stop.y do
-                {x = x; y = y}
-    |]
+    if isHorisontal line then
+        [| for x in line.start.x .. xstep .. line.stop.x -> {x = x; y = line.start.y} |]
+    elif isVertical line then
+        [| for y in line.start.y .. ystep .. line.stop.y -> {x = line.start.x; y = y} |]
+    else
+        let xs = [| for x in line.start.x .. xstep ..line.stop.x -> x |]
+        let ys = [| for y in line.start.y .. ystep ..line.stop.y -> y |]
+        Array.zip xs ys |> Array.map (fun v -> {x = fst v; y = snd v})
 
 let lines =
     System.IO.File.ReadAllLines "5.txt"
     |> Array.map toLine
-    |> Array.filter isHorizontalOrVertical
+
+let solve lines =
+    lines
     |> Array.map trajectory
     |> Array.collect id
     |> Array.countBy id
     |> Array.filter (fun v -> snd v >= 2)
     |> Array.length
-    |> debugs
+
+let ans1 =
+    lines
+    |> Array.filter isHorizontalOrVertical
+    |> solve
+    |> logs
+
+let ans2 =
+    lines
+    |> solve
+    |> logs
