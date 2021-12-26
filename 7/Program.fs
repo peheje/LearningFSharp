76b244ee0b1cd86dc68418adb603b492460c2202
Peter <peterhj@gmail.com>
2021-12-26 13:30:34 +0100

    7 part 1

---
+++ 7/Program.fs
@@ -0,0 +1,11 @@
+﻿let split c s = (s: string).Split(c: char)
+let crabs = System.IO.File.ReadAllText("7.txt") |> split ',' |> Seq.map int
+let positions = [0..crabs |> Seq.max]
+
+let cost position =
+    crabs |> Seq.sumBy (fun crab -> abs(crab - position))
+
+positions
+    |> Seq.map cost
+    |> Seq.min
+    |> printfn "%A"
