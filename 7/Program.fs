a3c7bd4ea274d5c9dfadd5c1439433a467438c48
Peter <peterhj@gmail.com>
2021-12-26 14:14:41 +0100

    part 2

--- 7/Program.fs
+++ 7/Program.fs
@@ -2,10 +2,22 @@
 let crabs = System.IO.File.ReadAllText("7.txt") |> split ',' |> Seq.map int
 let positions = [0..crabs |> Seq.max]
 
-let cost position =
-    crabs |> Seq.sumBy (fun crab -> abs(crab - position))
+// Part 1
+let cost1 position =
+    crabs |> Seq.sumBy (fun crab -> (crab - position) |> abs)
 
 positions
-    |> Seq.map cost
+    |> Seq.map cost1
+    |> Seq.min
+    |> printfn "%A"
+
+// Part 2
+let triangle n = (n*(n + 1))/2
+
+let cost2 position =
+    crabs |> Seq.sumBy (fun crab -> (crab - position) |> abs |> triangle)
+
+positions
+    |> Seq.map cost2
     |> Seq.min
     |> printfn "%A"
