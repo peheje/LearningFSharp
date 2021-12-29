1a47564548f1a30e7cfd08ca749ebaa34963bcf8
Peter <peterhj@gmail.com>
2021-12-29 17:48:11 +0100

    code in rules self for each digit

--- 8/Program.fs
+++ 8/Program.fs
@@ -2,87 +2,65 @@
 let json s = System.Text.Json.JsonSerializer.Serialize(s, System.Text.Json.JsonSerializerOptions(WriteIndented = true))
 let debugs s = System.IO.File.WriteAllText("debug.json", json s); s
 
-let numberToSegmentCount = Map [
-    (0, 6); (1, 2); (2, 5); (3, 5); (4, 4); (5, 5); (6, 6); (7, 3); (8, 7); (9, 6)
-]
-
-let uniqueSegmentCountToNumber = Map [
-    (2, 1); (4, 4); (3, 7); (7, 8)
-]
-
-let segmentMap = [|
-    [|1;2;3;5;6;7|];
-    [|3;6|];
-    [|1;3;4;5;7|];
-    [|1;3;4;6;7|];
-    [|2;3;4;6;|];
-    [|1;2;4;6;7|];
-    [|1;2;4;5;6;7|];
-    [|1;3;6|];
-    [|1;2;3;4;5;6;7|];
-    [|1;2;3;4;6;7|]
-|]
-
-let uniqueSegmentCounts = uniqueSegmentCountToNumber |> Map.keys |> Seq.toArray
-
 let split c a = (a:string).Split(c:string)
 
-let input = System.IO.File.ReadAllLines "sample.txt"
+let input = System.IO.File.ReadAllLines "8.txt"
 
 // Part 1
-
 let part1 =
     input
     |> Array.map (fun x -> x |> split " | " |> Array.item 1 |> split " ")
     |> Array.collect id
     |> Array.map (fun x -> x |> String.length)
-    |> Array.filter (fun c -> uniqueSegmentCounts |> Array.contains c)
+    |> Array.filter (fun c -> [|2;4;3;7|] |> Array.contains c)
     |> Array.length
+    |> logs
 
 // Part 2
+let deduce (patterns:string array) =
 
-let exclusivePairwise xs ys =
-    let rotate xs = Seq.append (xs |> Seq.removeAt 0) [(xs |> Seq.item 0)]
-    let rec rotator xs = seq {
-        yield xs
-        yield! xs |> rotate |> rotator
-    }
-    xs |> rotator |> Seq.map (fun xs -> xs |> Seq.zip ys) |> Seq.take (xs |> Seq.length)
+    let hasUniqueLength n =
+        patterns |> Seq.find (fun pattern -> pattern |> Seq.length = n)
 
-let mapper xs =
-    let segments = 
-        xs
-        |> Array.map (fun x -> (x |> String.length, x))
-        |> Array.map (fun (l, x) -> (uniqueSegmentCountToNumber |> Map.tryFind l, x))
-        |> Array.map (fun (n, x) -> (n, x, if Option.isSome n then segmentMap[n.Value] else [||]))
-        |> Array.map (fun (n, x, s) -> (n, x, exclusivePairwise x s))
-    
-    let (knowns, unknowns) =
-        segments
-        |> Array.partition (fun (n, _, _) -> Option.isSome n)
+    let one = hasUniqueLength 2
+    let four = hasUniqueLength 4
+    let seven = hasUniqueLength 3
+    let eight = hasUniqueLength 7
 
-    let rules =
-        knowns
-        |> Seq.map (fun (n, x, s) -> s)
+    let hasLength n =
+        patterns |> Seq.filter (fun pattern -> pattern |> Seq.length = n)
 
-    let flattenedRules = rules |> Seq.collect id |> Seq.rev
+    let hasInCommonWith b n a =
+        a |> Seq.filter (fun c -> b |> Seq.contains c) |> Seq.length = n
 
-    let mutable rulesLeft = flattenedRules
+    let five = hasLength 5 |> Seq.find (hasInCommonWith four 3)
 
-    for rule in flattenedRules do
-        for (number, character) in rule do
-            rulesLeft <- rulesLeft |> Seq.filter (fun xs -> 
-                xs |> Seq.exists (fun (i, c) -> i = number && c <> character)
-            )
+    let zero = hasLength 6 |> Seq.find (hasInCommonWith five 4)
 
-        logs rulesLeft
+    let two = "todo"
 
+    let three = "todo"
 
+    let six = "todo"
 
-    segments
+    let nine = "todo"
+
+    [
+        (0, zero);
+        (1, one);
+        (2, two);
+        (3, three);
+        (4, four);
+        (5, five);
+        (6, six);
+        (7, seven);
+        (8, eight);
+        (9, nine);
+    ]
 
 let part2 =
     input
     |> Array.map (fun x -> x |> split " | " |> Array.item 0 |> split " ")
-    |> Array.map mapper
+    |> Array.map deduce
+    |> debugs
 
