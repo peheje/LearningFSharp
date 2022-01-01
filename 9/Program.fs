a12fe825c1a0eef098320da4f15c9d3a23b63be9
peheje <peterhj@gmail.com>
2022-01-01 20:08:45 +0100

    use fold

--- 9/Program.fs
+++ 9/Program.fs
@@ -10,16 +10,18 @@ let rows = Array.length data
 let columns = Array.length data[0]
 let indexOk row col = row >= 0 && col >= 0 && row < rows && col < columns
 let get row col = if indexOk row col then data[row][col] else 100
-let surrounding row col =
+let neighbors row col =
     let left = get row (col-1)
     let right = get row (col+1)
     let below = get (row+1) col
     let above = get (row-1) col
     [left; right; below; above]
 
-[for (ri, ci) in Seq.allPairs [0..rows] [0..columns] do
-    let cur = get ri ci
-    let neighbors = surrounding ri ci
-    if neighbors |> Seq.forall (fun x -> x > cur) then cur + 1
-]
-|> Seq.sum |> logs
+let risk ri ci =
+    let c = get ri ci
+    if neighbors ri ci |> Seq.forall (fun x -> x > c) then c + 1
+    else 0
+
+Seq.allPairs [0..rows] [0..columns]
+|> Seq.fold (fun sum (row, column) -> sum + risk row column) 0
+|> logs
