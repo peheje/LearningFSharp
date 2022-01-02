ceb7d97376b114d3f2530ad89cb94a9951850c77
peheje <peterhj@gmail.com>
2022-01-02 20:06:38 +0100

    started 10

---
+++ 10/Program.fs
@@ -0,0 +1,21 @@
+﻿let log x = printfn "%A" x
+
+let s = "(((((((((())))))))))" |> Seq.toArray
+
+let opposite = Map [('{', '}'); ('<', '>'); ('[', ']'); ('(', ')')]
+let opposite2 = Seq.zip (opposite |> Map.values) (opposite |> Map.keys) |> Map.ofSeq
+
+let isCloser c = "})]>" |> Seq.contains c
+let isOpener c = not (isCloser c)
+
+s |> Array.fold (fun acc c ->
+    log acc
+    if isOpener c then
+        opposite[c]::acc
+    else
+        match acc with
+        | x::xs -> if opposite2[c] <> x then failwith "unmatched1" else xs
+        | [] -> failwith "unmatched2"
+
+) []
+|> log
