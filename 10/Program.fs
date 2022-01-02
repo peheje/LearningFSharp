4b3f23b6cf9aa22be62edc1c853c1fc0547780bc
peheje <peterhj@gmail.com>
2022-01-02 20:16:21 +0100

    replace

--- 10/Program.fs
+++ 10/Program.fs
@@ -1,6 +1,7 @@
 ﻿let log x = printfn "%A" x
 
-let s = "(((((((((())))))))))" |> Seq.toArray
+let remove a s = (s:string).Replace(a, "")
+let s = "(((((((((())))))))))"
 
 let opposite = Map [('{', '}'); ('<', '>'); ('[', ']'); ('(', ')')]
 let opposite2 = Seq.zip (opposite |> Map.values) (opposite |> Map.keys) |> Map.ofSeq
@@ -8,14 +9,10 @@ let opposite2 = Seq.zip (opposite |> Map.values) (opposite |> Map.keys) |> Map.o
 let isCloser c = "})]>" |> Seq.contains c
 let isOpener c = not (isCloser c)
 
-s |> Array.fold (fun acc c ->
-    log acc
-    if isOpener c then
-        opposite[c]::acc
-    else
-        match acc with
-        | x::xs -> if opposite2[c] <> x then failwith "unmatched1" else xs
-        | [] -> failwith "unmatched2"
+let corrupted
+let mutable ms = s
+for i in 0 .. Seq.length s do
+    ms <- ms |> remove "()"
 
-) []
-|> log
+
+log ms
