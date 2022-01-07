ee4c2d3844709ba7e7efd6ca3e6b72abba97671f
Peter <peterhj@gmail.com>
2022-01-07 20:21:50 +0100

    part 2, huh not right answer

--- 10/Program.fs
+++ 10/Program.fs
@@ -1,7 +1,6 @@
-﻿let log x = printfn "%A" x
+﻿let logs x = printfn "%A" x; x
 let reverse c = match c with | '[' -> ']' | '(' -> ')' | '{' -> '}' | '<' -> '>' | _ -> failwith "unknown reverse"
 let point1 c = match c with | ')' -> 3 | ']' -> 57 | '}' -> 1197 | '>' -> 25137 | _ -> failwith "unknown point"
-let point2 c = match c with | ')' -> 1 | ']' -> 2 | '}' -> 3 | '>' -> 4 | _ -> failwith "unknown"
 let isStarter c = ['['; '('; '{'; '<'] |> Seq.contains c
 
 let findUnexpected xs =
@@ -17,19 +16,32 @@ let findUnexpected xs =
                 | s::sx -> if s = x then loop sx rest else (Some x, stack)
     loop List.empty (xs |> Seq.toList)
 
-let input = System.IO.File.ReadAllLines "data.txt"
+let input = System.IO.File.ReadAllLines "sample.txt"
 
 // Part 1
-input
-|> Seq.map (findUnexpected >> fst)
-|> Seq.choose id
-|> Seq.map point1
-|> Seq.sum
-|> log
+let part1 = 
+    input
+    |> Seq.map (findUnexpected >> fst)
+    |> Seq.choose id
+    |> Seq.map point1
+    |> Seq.sum
+    |> logs
 
 // Part 2
-input
-|> Seq.map findUnexpected
-|> Seq.filter (fst >> Option.isNone)
-|> Seq.toArray
-|> log
+let point2 c = match c with | ')' -> 1 | ']' -> 2 | '}' -> 3 | '>' -> 4 | _ -> failwith "unknown"
+
+let score xs =
+    xs
+    |> Seq.map point2
+    |> Seq.fold (fun acc x -> acc * 5 + x) 0
+
+let part2 =
+    input
+    |> Seq.map findUnexpected
+    |> Seq.filter (fst >> Option.isNone)
+    |> Seq.map (snd >> score)
+    |> Seq.sort
+    |> Seq.splitInto 2
+    |> Seq.item 0
+    |> Seq.last
+    |> logs
