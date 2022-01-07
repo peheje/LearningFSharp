55f4f9bc994c1f066c753d1e087240c239e46060
Peter <peterhj@gmail.com>
2022-01-07 19:03:46 +0100

    put loop into function

--- 10/Program.fs
+++ 10/Program.fs
@@ -3,18 +3,18 @@ let reverse c = match c with | '[' -> ']' | '(' -> ')' | '{' -> '}' | '<' -> '>'
 let point c = match c with | ')' -> 3 | ']' -> 57 | '}' -> 1197 | '>' -> 25137 | _ -> failwith "unknown point"
 let isStarter c = ['['; '('; '{'; '<'] |> Seq.contains c
 
-let rec loop stack xs =
-    match xs with
-    | [] -> None
-    | x::rest ->
-        if isStarter x then
-            loop (reverse x::stack) rest
-        else
-            match stack with
-            | [] -> Some x
-            | s::sx -> if s = x then loop sx rest else Some x
-
-let findUnexpected xs = loop List.empty (xs |> Seq.toList)
+let findUnexpected xs =
+    let rec loop stack xs =
+        match xs with
+        | [] -> None
+        | x::rest ->
+            if isStarter x then
+                loop (reverse x::stack) rest
+            else
+                match stack with
+                | [] -> Some x
+                | s::sx -> if s = x then loop sx rest else Some x
+    loop List.empty (xs |> Seq.toList)
 
 let input = System.IO.File.ReadAllLines "data.txt"
 
