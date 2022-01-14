ca59d24869ac80de59e38107fef198a24c0f8260
peheje <peterhj@gmail.com>
2022-01-14 21:34:18 +0100

    move repository to a general learning F# repo

---
+++ Async/Program.fs
@@ -0,0 +1,53 @@
+﻿open System
+
+let dump data =
+    async {
+        let path = $"dump/{Guid.NewGuid ()}.txt"
+        do! IO.File.WriteAllTextAsync (path, data) |> Async.AwaitTask
+    }
+
+let limitString maxLength s =
+    let lastIndex = (s |> String.length) - 1
+    let limit = min lastIndex maxLength
+    s.Substring (0, limit)
+
+let getUrl url =
+    async {
+        try
+            let! response = FSharp.Data.Http.AsyncRequestString (url, timeout = 5000)
+            return Some response
+        with
+        | ex ->
+            let message = ex.Message |> limitString 50
+            printfn $"Failed to download {url} exception message {message}"
+            return None
+    }
+
+let readUrlsList () =
+    async {
+        let path = "urls.txt"
+        return! IO.File.ReadAllLinesAsync (path) |> Async.AwaitTask
+    }
+
+let mapOptionalAsync f asyncItem =
+    async {
+        match! asyncItem with
+        | Some x ->
+            let! result = f x
+            return Some result
+        | None -> return None
+    }
+
+printfn "start"
+
+async {
+    let! urls = readUrlsList ()
+
+    do!
+        urls
+        |> Seq.map getUrl
+        |> Seq.map (mapOptionalAsync dump)
+        |> Async.Parallel
+        |> Async.Ignore
+}
+|> Async.RunSynchronously
