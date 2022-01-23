46f1ba1f637fff0489cd970dd56cd10e8d5fb92c
PEHEJE <peterhj@gmail.com>
2022-01-23 15:53:51 +0000

    bit better but still mix of mutable state, just seems right compared to the non-mutable solutions ive seen

--- AOC2021/11/Program.fs
+++ AOC2021/11/Program.fs
@@ -1,5 +1,5 @@
 ﻿let logs x = printfn "%A" x; x
-let rows = System.IO.File.ReadAllLines "sample.txt"
+let rows = System.IO.File.ReadAllLines "data.txt"
 let split s = [|for c in s -> int (string c)|]
 let octie = rows |> Array.map split
 let lastIndex = Array.length octie - 1
@@ -26,39 +26,27 @@ let surrounding row col =
 let increment xxs =
     xxs |> map (fun x -> x + 1)
 
-type Octo = { value: int; ri: int; ci: int }
+let mutable flashes = 0
 
-let neighbors octie ri ci =
-    let indexes = surrounding ri ci
-    octie |> Seq.filter (fun o -> indexes |> Array.contains (o.ri, o.ci))
-
-let rec flashingLoop xxs flashed =
-    let toFlash, rest = xxs |> Array.partition (fun o -> o.value > 9 && not (flashed |> Array.contains (o.ri, o.ci)))
-
-    if toFlash |> Array.isEmpty then
-        xxs
-    else
-        let flashedIndexes = toFlash |> Array.map (fun o -> o.ri, o.ci)
-        let incrementTheseIndexes = toFlash |> Array.map (fun o -> surrounding o.ri o.ci) |> Array.collect id
-
-        let incrementedOctoes =
-            rest
-            |> Array.filter (fun o -> incrementTheseIndexes |> Array.contains (o.ri, o.ci))
-            |> Array.map (fun o -> { o with value = o.value + 1 })
-
-        let totalFlashed = (Array.concat [|flashed; flashedIndexes|])
-        flashingLoop incrementedOctoes totalFlashed
-
-
-let flashing xxs =
-    let octieSeq = xxs |> mapi (fun v ri ci -> { value = v; ri = ri; ci = ci }) |> Array.collect id
-    let octie = flashingLoop octieSeq Array.empty
+let rec flashing xxs =
+    for ri, xs in xxs |> Array.indexed do
+        for ci, v in xs |> Array.indexed do
+            if v > 9 then
+                let neighbors = surrounding ri ci
+                for nri, cri in neighbors do
+                    let neighbor = xxs[nri][cri]
+                    if neighbor >= 0 then
+                        xxs[nri][cri] <- neighbor + 1
+                xxs[ri][ci] <- -1
+                flashes <- flashes + 1
     
-    octie |> 
-
+    if xxs |> Array.exists (fun xs -> xs |> Array.exists (fun x -> x > 9)) then
+        flashing xxs
+    else
+        xxs
 
 let reset xxs =
-    xxs |> mapic (fun x _ _ -> x > 9) (fun x _ _-> 0)
+    xxs |> mapic (fun x _ _ -> x > 9 || x < 0) (fun x _ _-> 0)
 
 let step = increment >> flashing >> reset
 
@@ -68,4 +56,9 @@ let rec next octie = seq {
     yield! octie |> step |> next
 }
 
-octie |> next |> Seq.item 1 |> logs
+let output = 
+    octie
+    |> next
+    |> Seq.item 100
+
+logs flashes
