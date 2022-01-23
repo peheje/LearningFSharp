364ebd4c04eef0e0b70ee9f15d32d5a53b83b941
PEHEJE <peterhj@gmail.com>
2022-01-23 16:06:48 +0000

    refactor

--- AOC2021/11/Program.fs
+++ AOC2021/11/Program.fs
@@ -16,27 +16,29 @@ let surrounding row col =
     Array.allPairs (maxMin row) (maxMin col)
     |> Array.filter ((<>) (row, col))
 
-let increment xxs = xxs |> map (fun x -> x + 1)
+let increment xxs = xxs |> map ((+) 1)
 
+let flashedThisStep = -1
 let mutable flashes = 0
 let rec flashing xxs =
     for ri, xs in xxs |> Array.indexed do
         for ci, v in xs |> Array.indexed do
             if v > 9 then
+                xxs[ri][ci] <- flashedThisStep
+                flashes <- flashes + 1
+
                 let neighbors = surrounding ri ci
                 for nri, nci in neighbors do
                     let neighbor = xxs[nri][nci]
-                    if neighbor <> -1 then
+                    if neighbor <> flashedThisStep then
                         xxs[nri][nci] <- neighbor + 1
-                xxs[ri][ci] <- -1
-                flashes <- flashes + 1
-    
+
     if xxs |> Array.exists (fun xs -> xs |> Array.exists (fun x -> x > 9)) then
         flashing xxs
     else
         xxs
 
-let reset xxs = xxs |> mapic (fun x -> x > 9 || x = -1) (fun _ -> 0)
+let reset xxs = xxs |> mapic (fun x -> x > 9 || x = flashedThisStep) (fun _ -> 0)
 
 let step = increment >> flashing >> reset
 
