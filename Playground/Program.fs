22461ae16519d5dcd11491720ce23e9662173fa7
PEHEJE <peterhj@gmail.com>
2022-01-31 19:30:23 +0100

    unfold example fib

---
+++ Playground/Program.fs
@@ -0,0 +1,10 @@
+﻿let generator state =
+    let first, second = state
+    let nextFibNumber = first + second
+    let nextState = (second, nextFibNumber)
+    Some (nextFibNumber, nextState)
+
+let fib =
+    Seq.unfold generator (1, 1)
+    |> Seq.take 10
+    |> Seq.toList
