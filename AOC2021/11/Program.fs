1276180ce2867c6b2693726ab3829e846fbc963d
PEHEJE <peterhj@gmail.com>
2022-02-13 21:02:38 +0100

    format

--- AOC2021/11/Program.fs
+++ AOC2021/11/Program.fs
@@ -15,7 +15,7 @@ let anyFlashing data =
 
 let surroundingIndices row col =
     let neighbors = [|[|-1; -1|]; [|-1; 0|]; [|-1; 1|]; [|0; -1|]; [|0; 1|]; [|1; -1|]; [|1; 0|]; [|1; 1|]|]
-    [|for neighbor in neighbors -> toIndex(neighbor[0] + row) (neighbor[1] + col)|]
+    [|for neighbor in neighbors -> toIndex (neighbor[0] + row) (neighbor[1] + col)|]
     |> Array.filter (fun index -> index >= 0 && index < length)
 
 let mutable flashes = 0
