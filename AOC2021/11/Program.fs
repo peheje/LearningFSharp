060a0d52fa0e278b57d887c930b9f8904bbc5a03
PEHEJE <peterhj@gmail.com>
2022-02-13 21:18:02 +0100

    refactor

--- AOC2021/11/Program.fs
+++ AOC2021/11/Program.fs
@@ -9,11 +9,13 @@ let size = lines |> Array.length
 let length = octos |> Array.length
 
 let toIndex row col = row * size + col
+let fromIndex index = (index/size, index % size)
 
 let anyFlashing data =
     data |> Array.exists(fun x -> x > 9)
 
-let surroundingIndices row col =
+let surroundingIndices index =
+    let (row, col) = fromIndex index
     let neighbors = [|(-1, -1); (-1, 0); (-1, 1); (0, -1); (0, 1); (1, -1); (1, 0); (1, 1)|]
     [|for (ri, ci) in neighbors -> toIndex (ri + row) (ci + col)|]
     |> Array.filter (fun index -> index >= 0 && index < length)
@@ -25,15 +27,13 @@ for _ in 1..99 do
         octos[i] <- octos[i] + 1
 
     while anyFlashing octos do
-        for i in 0..size-1 do
-            for j in 0..size-1 do
-                let index = toIndex i j
-                if octos[index] > 9 then
-                    flashes <- flashes + 1
-                    octos[index] <- -1
-                    for ni in surroundingIndices i j do
-                        if octos[ni] <> -1 then
-                            octos[ni] <- octos[ni] + 1
+        for i in 0..length-1 do
+            if octos[i] > 9 then
+                flashes <- flashes + 1
+                octos[i] <- -1
+                for ni in surroundingIndices i do
+                    if octos[ni] <> -1 then
+                        octos[ni] <- octos[ni] + 1
 
     for i in 0..length-1 do
         if octos[i] = -1 then
