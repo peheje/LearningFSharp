e74b146d62b07babfa1da8b32fee76b5683fcd28
peheje <peterhj@gmail.com>
2022-02-15 10:01:31 +0100

    create helpers for reset and increment

--- AOC2021/11_new/11.fsx
+++ AOC2021/11_new/11.fsx
@@ -22,6 +22,10 @@ let isNeighbors a b =
       (1, 1) ]
     |> Seq.exists (fun (r, c) -> b.row = a.row + r && b.col = a.col + c)
 
+let increment octo = { octo with value = octo.value + 1 }
+
+let reset octo = { octo with value = 0 }
+
 let rec flash octos flashed =
     let toFlash = octos |> List.tryFind (fun o -> o.value > 9)
 
@@ -35,29 +39,23 @@ let rec flash octos flashed =
             |> List.filter (fun o -> o <> f)
             |> List.map (fun o ->
                 if isNeighbors f o then
-                    { o with value = o.value + 1 }
+                    increment o
                 else
                     o)
 
         flash octos flashed
 
 let rec step octos count iteration =
-    let octos =
-        octos
-        |> List.map (fun o -> { o with value = o.value + 1 })
-
+    let octos = octos |> List.map increment
     let (octos, flashed) = flash octos List.empty
 
     if iteration = 100 then
         count
     else
-        let flashed =
-            flashed
-            |> List.map (fun o -> { o with value = 0 })
-
-        let flashCount = flashed |> List.length
+        let flashed = flashed |> List.map reset
+        let count = count + (flashed |> List.length)
         let octos = List.append flashed octos
-        step octos (count + flashCount) (iteration + 1)
+        step octos count (iteration + 1)
 
 let stepCount = step octos 0 0
 
