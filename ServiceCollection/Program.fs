7984241aa4b23ef563aa3731bb3be4ac61fc2bbe
PEHEJE <peterhj@gmail.com>
2022-03-08 21:05:33 +0100

    split into multiple files

--- ServiceCollection/Program.fs
+++ ServiceCollection/Program.fs
@@ -1,28 +1,13 @@
-﻿open Microsoft.Extensions.DependencyInjection
+﻿open Models.Customer
+open Services
 
 let serialize object =
     System.Text.Json.JsonSerializer.Serialize(object)
 
-type ICustomer =
-    abstract Name : string
-    abstract Age : int
-    abstract Legal : unit -> bool
-
-let createCustomer name age =
-    { new ICustomer with
-        member this.Name = name
-        member this.Age = age
-        member this.Legal () = this.Age > 18
-    }
-
-let services = ServiceCollection()
-
-services.AddTransient<ICustomer>((fun provider -> createCustomer "Peter" 17)) |> ignore
-
-let provider = services.BuildServiceProvider()
-
-let customer = provider.GetService<ICustomer>()
+let customer = getService<ICustomer> ()
 
 printfn "%A" (serialize customer)
 
 printfn "%A" (customer.Legal())
+
+printfn "%A" (getInstancesCount ())
