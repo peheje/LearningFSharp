b8a3aafeb9bff22309e1c3414e84adbf23066ada
peheje <peterhj@gmail.com>
2022-05-07 23:35:33 +0200

    use benchmark dotnet

--- Multiply/Program.fs
+++ Multiply/Program.fs
@@ -1,47 +1,38 @@
-﻿let benchmark name code =
-    let sw = System.Diagnostics.Stopwatch.StartNew()
-    code()
-    let elapsed = sw.ElapsedMilliseconds
-    printfn "CPU time [%s] %ims" name elapsed
+﻿open BenchmarkDotNet.Attributes
+open BenchmarkDotNet.Running
 
-let multiply2 a b =
+let even x = x % 2 <> 0
+
+let multiply1 a b =
     let mutable result = 0
     let mutable left = a
     let mutable right = b
     while left <> 0 do
-        if left % 2 <> 0 then
+        if even left then
             result <- result + right
         left <- left / 2
         right <- right * 2
     result
 
-let multiply a b =
+let multiply2 a b =
     let rec loop left right result =
         let nextLeft = left / 2
         let nextRight = right * 2
         if left = 0 then
             result
-        elif left % 2 <> 0 then
+        elif even left then
             loop nextLeft nextRight (result + right)
         else
             loop nextLeft nextRight result
     loop a b 0
 
-multiply 4678 231452 |> printfn "%A"
+[<MemoryDiagnoser>]
+[<ShortRunJob>]
+type MultiplyComparison () =
+    [<Benchmark>]
+    member self.MutableMutate () = multiply1 234567 245
 
-// = 1082732456
+    [<Benchmark>]
+    member self.RecursiveLoop () = multiply2 234567 245
 
-let size = 100_000_000
-let mutable y = 0.0
-benchmark "multiply" (fun () -> 
-    for _ = 0 to size do
-        y <- y + (multiply 4678 231452 |> float)
-)
-printfn "%f" y
-
-y <- 0
-benchmark "*" (fun () -> 
-    for _ = 0 to size do
-        y <- y + (4678 * 231452 |> float)
-)
-printfn "%f" y
+let runner = BenchmarkRunner.Run<MultiplyComparison>()
