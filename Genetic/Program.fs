40ecf9638c44672d38fb8a96530949b715096c28
PEHEJE <peterhj@gmail.com>
2022-05-08 16:41:28 +0200

    not working

--- Genetic/Program.fs
+++ Genetic/Program.fs
@@ -1,27 +1,33 @@
 ﻿let rand () = System.Random.Shared.NextDouble()
 let randRange min max = rand () * (max - min) + min
 let binarySearch target xs = System.Array.BinarySearch (xs, target) |> abs
-let mutatePower () = randRange 0.0 5.0
-let mutateOdds () = rand () < 0.1
+let mutatePower () = randRange -1.0 1.0
+let mutateOdds () = rand () < 0.05
+let crossoverPower () = rand () < 0.05
+let crossoverOdds () = rand () < 0.05
+
+type Agent = { data: float array; mutable fitness: float }
+
 let poolSize = 1000
-
-let crossoverPower () = rand () < 0.1
-let crossoverOdds () = rand () < 0.1
-
-type Agent = { data: float array; fitness: float }
-
-let min, max = 0.0, 10.0
-let target = [|0.0; 0.0; 1.0; 1.0; 2.0; 2.0; 3.0; 3.0; 4.0; 4.0; 5.0; 5.0; 6.0; 6.0; 7.0; 7.0; 8.0; 8.0; 9.0; 9.0|]
+let generations = 100_000
+let print = 10_000
+let low, high = 0.0, 10.0
+let clamp x = System.Math.Clamp(x, low, high)
+//let target = [|0.0; 0.0; 1.0; 1.0; 2.0; 2.0; 3.0; 3.0; 4.0; 4.0; 5.0; 5.0; 6.0; 6.0; 7.0; 7.0; 8.0; 8.0; 9.0; 9.0|]
+let target = [|1.0; 0.0|]
 let argsize = target |> Array.length
 
 let calculateFitness (data: float array) =
-    let mutable fit = 0.0
+    let mutable totalError = 0.0
     for i in 0..argsize-1 do
-        fit <- fit + ((data[i] - target[i]) |> abs)
-    1.0 / fit
+        let value = data[i]
+        let target = target[i]
+        let error = (value - target) |> abs
+        totalError <- totalError + error
+    100.0 / totalError    
 
 let createAgent () =
-    let data = Array.init argsize (fun _ -> randRange min max)
+    let data = Array.init argsize (fun _ -> randRange low high)
     { data = data; fitness = calculateFitness data }
 
 let createWheel agents =
@@ -35,12 +41,13 @@ let pickIndex wheel =
     binarySearch roll wheel - 1
 
 let pick xs wheel =
-    Array.get xs (pickIndex wheel)
+    let idx = pickIndex wheel
+    Array.get xs idx
 
 let mutate agent =
     for i in 0..argsize-1 do
         if mutateOdds () then
-            agent.data[i] <- agent.data[i] + mutatePower ()
+            agent.data[i] <- (agent.data[i] + mutatePower ()) |> clamp
 
 let crossover agent pool wheel =
     let mate = pick pool wheel
@@ -50,4 +57,16 @@ let crossover agent pool wheel =
             agent.data[i] <- mate.data[i]
             mate.data[i] <- tmp
 
-let pool = Array.init poolSize (fun _ -> createAgent ())
+let mutable pool = Array.init poolSize (fun _ -> createAgent ())
+
+for generation in 0..generations do
+    let wheel = createWheel pool
+    for agent in pool do
+        if crossoverOdds () then crossover agent pool wheel
+        if mutateOdds () then mutate agent
+        agent.fitness <- calculateFitness agent.data
+    pool <- Array.init poolSize (fun _ -> pick pool wheel)
+    
+    if generation % print = 0 || generation = generations then
+        let best = pool |> Array.maxBy (fun a -> a.fitness)
+        printfn "best agent is %A" best
