00d8c15d656b0cb9c78c00a06cd7e25e26507cff
peheje <peterhj@gmail.com>
2022-05-12 21:48:37 +0200

    add the file

---
+++ Async/DifferentReturnParallelTask.fs
@@ -0,0 +1,36 @@
+module Learning.DifferentTypeParallelTask
+
+open System.IO
+open System.Threading.Tasks
+
+let private readTheFile () =
+    task {
+        let path = "/Users/phj/Desktop/AS/data.txt"
+        do! Task.Delay 2000
+        return! File.ReadAllTextAsync path
+    }
+
+let private doSomeWork () =
+    task {
+        do! Task.Delay 2000
+        return 42
+    }
+
+let run () =
+    printfn "started"
+
+    let mainTask = task {
+        let fileAsync = readTheFile ()
+        let answerAsync = doSomeWork ()
+
+        let! file = fileAsync
+        let! answer = answerAsync
+
+        printfn "%s" file
+        printfn "%i" answer
+        
+    }
+
+    mainTask.Wait ()
+
+    printfn "ended"
