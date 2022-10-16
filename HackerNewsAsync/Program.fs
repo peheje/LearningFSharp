ed7df4cec539480c08dfcfdd16be536f2d6e79e6
PEHEJE <peterhj@gmail.com>
2022-10-16 14:14:44 +0200

    refactor

--- HackerNewsAsync/Program.fs
+++ HackerNewsAsync/Program.fs
@@ -15,33 +15,42 @@ type Story =
 let client =
     new HttpClient(new SocketsHttpHandler(PooledConnectionLifetime = TimeSpan.FromMinutes(2)))
 
-let getStory (id: int) =
-    client.GetFromJsonAsync<Story>($"https://hacker-news.firebaseio.com/v0/item/{id}.json").Result
+let getStory id =
+    let url = $"https://hacker-news.firebaseio.com/v0/item/{id}.json"
+    client.GetFromJsonAsync<Story>(url).Result
 
 let sw = Stopwatch.StartNew()
 
-let topStoriesPath = "https://hacker-news.firebaseio.com/v0/topstories.json"
-let topStories = client.GetFromJsonAsync<int array>(topStoriesPath).Result |> Seq.take 20
-let queue = new BlockingCollection<int>()
-let results = ConcurrentDictionary<int, Story>()
-let producer = async {
-    for id in topStories do
-        queue.Add(id)
-    queue.CompleteAdding()
-}
+let topStories =
+    let topStoriesPath = "https://hacker-news.firebaseio.com/v0/topstories.json"
+    client.GetFromJsonAsync<int array>(topStoriesPath).Result
+        |> Seq.take 20
 
-let consumer = async {
-    try
-        while true do
-            let story = queue.Take(Threading.CancellationToken.None) |> getStory
-            results.TryAdd(story.id, story) |> Debug.Assert
-            printfn "Thread %i Received %s" Threading.Thread.CurrentThread.ManagedThreadId story.title
-    with
+let queue = new BlockingCollection<int>(1)
+let results = ConcurrentDictionary<int, Story>()
+
+let producer =
+    async {
+        for id in topStories do
+            queue.Add(id)
+
+        queue.CompleteAdding()
+        printfn "Producer finished"
+    }
+
+let consumer =
+    async {
+        try
+            while true do
+                let story = queue.Take(Threading.CancellationToken.None) |> getStory
+                results.TryAdd(story.id, story) |> Debug.Assert
+                printfn "Thread %i Received %s" Threading.Thread.CurrentThread.ManagedThreadId story.title
+        with
         | :? InvalidOperationException -> printfn "Consumer ended"
-}
+    }
 
 let maxConcurrent = 8
-let consumers = [for _ in 0..maxConcurrent -> consumer]
+let consumers = [ for _ in 0..maxConcurrent -> consumer ]
 
 (producer :: consumers)
 |> Async.Parallel
@@ -49,31 +58,7 @@ let consumers = [for _ in 0..maxConcurrent -> consumer]
 |> ignore
 
 results.Values
-    |> Seq.filter (fun s -> s.typ = "story")
-    |> Seq.sortBy (fun s -> s.id)
-    |> printfn "%A"
-
-(*
-
-let throttle = new Threading.SemaphoreSlim(10)
-
-let storiesJson =
-    topStories
-    |> Array.take 40
-    |> Array.Parallel.collect (fun id ->
-        try
-            throttle.Wait()
-            [| (getStory id) |]
-        finally
-            throttle.Release() |> ignore)
-    |> Array.filter (fun story -> story.typ = "story")
-    |> Array.take 30
-    |> Array.sortBy (fun story -> story.id)
-    |> JsonSerializer.Serialize
-
-sw.Stop()
-
-printfn "Took %ims" sw.ElapsedMilliseconds
-
-System.IO.File.WriteAllText("/Users/phj/Code/F-Sharp-Advent-of-Code-2021/HackerNewsAsync/data.json", storiesJson)
-*)
+|> Seq.filter (fun s -> s.typ = "story")
+|> Seq.sortBy (fun s -> s.id)
+|> Seq.length
+|> printfn "%A"
