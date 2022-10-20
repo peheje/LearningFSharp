7b843036208305ee50ebf75803e21f79acf950e7
PEHEJE <peterhj@gmail.com>
2022-10-20 22:02:54 +0200

    put the channel inside a module to encapsulate the sending/receiving

--- HackerNewsAsync/Program.fs
+++ HackerNewsAsync/Program.fs
@@ -3,34 +3,49 @@ open System.Diagnostics
 open System.Collections.Concurrent
 open HackerNewsAsync.Model
 open HackerNewsAsync
+open System.Threading
 open System.Threading.Channels
 
 let sw = Stopwatch.StartNew()
-let channel = Channel.CreateBounded<int>(1)
+
+module IdChannel =
+    let private channel = Channel.CreateBounded<int>(1)
+
+    // In future might use this: https://github.com/fsharp/fslang-design/blob/main/RFCs/FS-1021-value-task-interop.md
+    let receive () =
+        channel.Reader.ReadAsync().AsTask()
+        |> Async.AwaitTask
+
+    let send id =
+        channel.Writer.WriteAsync(id).AsTask()
+        |> Async.AwaitTask
+
+    let close () = channel.Writer.Complete()
 
 let producer =
     async {
         let! ids = HnClient.getTopStoriesIds 1000
 
         for id in ids do
-            do! channel.Writer.WriteAsync(id).AsTask() |> Async.AwaitTask // In future might use this: https://github.com/fsharp/fslang-design/blob/main/RFCs/FS-1021-value-task-interop.md
+            do! IdChannel.send (id)
 
-        channel.Writer.Complete()
+        IdChannel.close ()
     }
 
 let stories = ConcurrentDictionary<int, Story>()
 
 let consumer =
     async {
-        let threadId = Threading.Thread.CurrentThread.ManagedThreadId
+        let threadId = Thread.CurrentThread.ManagedThreadId
 
         try
             while true do
-                let! storyId = (channel.Reader.ReadAsync().AsTask()) |> Async.AwaitTask
+                let! storyId = IdChannel.receive ()
                 let! story = storyId |> HnClient.getStory
                 stories.TryAdd(story.id, story) |> Debug.Assert
                 printfn "Thread %i Received %s" threadId story.title
-        with :? AggregateException as ax ->
+        with
+        | :? AggregateException as ax ->
             for inner in ax.InnerExceptions do
                 if not (inner :? ChannelClosedException) then
                     raise inner
@@ -40,7 +55,10 @@ let consumer =
 
 async {
     let! p = Async.StartChild producer
-    let! consumers = List.init 8 (fun _ -> Async.StartChild consumer) |> Async.Parallel
+
+    let! consumers =
+        List.init 8 (fun _ -> Async.StartChild consumer)
+        |> Async.Parallel
 
     do! p
 
