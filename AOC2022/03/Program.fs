b24a7435b7c17eb362fc10530eb909edb31a686e
PEHEJE <peterhj@gmail.com>
2022-12-07 20:56:39 +0100

    refactor

--- AOC2022/03/Program.fs
+++ AOC2022/03/Program.fs
@@ -1,15 +1,9 @@
 ﻿let path = "C:\Users\peter\Repos\LearningFSharp\AOC2022\03\input"
-let raw = System.IO.File.ReadAllLines(path)
 
 let duplicate xs ys =
     let s1 = xs |> Set.ofSeq
     let s2 = ys |> Set.ofSeq
     Set.intersect s1 s2 |> Set.maxElement
-
-let intersects = raw |> Array.map (fun row ->
-    let s = row |> Seq.splitInto 2 |> Seq.toArray
-    duplicate s[0] s[1]
-)
 
 let priority (c: char) =
     if System.Char.IsLower c then
@@ -17,4 +11,10 @@ let priority (c: char) =
     else
         int c - 38
 
-intersects |> Array.map priority |> Array.sum
+let p1 =
+    System.IO.File.ReadAllLines(path)
+    |> Array.map (fun row ->
+        let s = row |> Seq.splitInto 2 |> Seq.toArray
+        duplicate s[0] s[1])
+    |> Array.map priority
+    |> Array.sum
