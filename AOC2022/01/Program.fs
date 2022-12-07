cffde087a015b02f15443953a8772891cecabf8d
Peter <peterhj@gmail.com>
2022-12-07 08:47:01 +0100

    Solution 01

---
+++ AOC2022/01/Program.fs
@@ -0,0 +1,14 @@
+﻿let path =
+    "/Users/peterhelstrupjensen/repos/LearningFSharp/AOC2022/01/input"
+
+let pt1 =
+    System.IO.File.ReadAllText(path).Split("\n\n")
+    |> Array.map (fun x -> x.Split("\n") |> Array.map int |> Array.sum)
+    |> Array.max
+
+let pt2 =
+    System.IO.File.ReadAllText(path).Split("\n\n")
+    |> Array.map (fun x -> x.Split("\n") |> Array.map int |> Array.sum)
+    |> Array.sortDescending
+    |> Array.take 3
+    |> Array.sum
