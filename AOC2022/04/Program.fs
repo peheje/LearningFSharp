4b1a3a1d3a45cb08a21ff6b08a34e0d125496585
PEHEJE <peterhj@gmail.com>
2022-12-07 21:25:46 +0100

    part 1

---
+++ AOC2022/04/Program.fs
@@ -0,0 +1,23 @@
+﻿let contains a b =
+    let x = a |> Set.ofList
+    let y = b |> Set.ofList
+    Set.isSubset x y || Set.isSubset y x
+
+let range (section: string) =
+    let s = section.Split("-")
+    let start = s[0] |> int
+    let stop = s[1] |> int
+    [start..stop]
+
+let path = "C:\Users\peter\Repos\LearningFSharp\AOC2022\04\input"
+let raw = System.IO.File.ReadAllLines path
+
+let p1 =
+    raw |> Array.map (fun row ->
+        let s = row.Split(",")
+        let e1 = s[0] |> range
+        let e2 = s[1] |> range
+        contains e1 e2
+    )
+    |> Array.filter id
+    |> Array.length
