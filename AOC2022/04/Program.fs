440888e605c0f72a3d84ebcaef1e37435c0b48a8
PEHEJE <peterhj@gmail.com>
2022-12-07 21:31:28 +0100

    probably part 2, DDOS on AOC2022

--- AOC2022/04/Program.fs
+++ AOC2022/04/Program.fs
@@ -1,23 +1,38 @@
-﻿let contains a b =
+﻿let fullyContains (a, b) =
     let x = a |> Set.ofList
     let y = b |> Set.ofList
     Set.isSubset x y || Set.isSubset y x
+
+let containsAny (a, b) =
+    let x = a |> Set.ofList
+    let y = b |> Set.ofList
+    Set.intersect x y |> Set.isEmpty |> not
 
 let range (section: string) =
     let s = section.Split("-")
     let start = s[0] |> int
     let stop = s[1] |> int
-    [start..stop]
+    [ start..stop ]
 
 let path = "C:\Users\peter\Repos\LearningFSharp\AOC2022\04\input"
 let raw = System.IO.File.ReadAllLines path
 
-let p1 =
-    raw |> Array.map (fun row ->
+let ranges =
+    raw
+    |> Array.map (fun row ->
         let s = row.Split(",")
         let e1 = s[0] |> range
         let e2 = s[1] |> range
-        contains e1 e2
-    )
+        e1, e2)
+
+let p1 =
+    ranges
+    |> Array.map fullyContains
+    |> Array.filter id
+    |> Array.length
+
+let p2 =
+    ranges
+    |> Array.map containsAny
     |> Array.filter id
     |> Array.length
