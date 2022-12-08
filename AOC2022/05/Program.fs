ab52251744d2ea9a7c972cc4e220af60d239805b
peheje <peterhj@gmail.com>
2022-12-08 21:14:45 +0100

    cleanup

--- AOC2022/05/Program.fs
+++ AOC2022/05/Program.fs
@@ -1,7 +1,6 @@
 ﻿let path = "/Users/phj/Code/F-Sharp-Advent-of-Code-2021/AOC2022/05/input"
-let raw = System.IO.File.ReadAllText path
-
-let s = raw.Split "\n\n"
+let raw = (System.IO.File.ReadAllText path).Split("\n\n")
+let cratesRaw, movesRaw = raw[0], raw[1]
 
 let parseCrates (row: string) =
     row
@@ -11,21 +10,15 @@ let parseCrates (row: string) =
         .Replace(" ", "")
     |> Seq.toArray
 
-let makeMove source destination =
-    match source with
-    | toMove :: tailOfSource ->
-        (tailOfSource, toMove :: destination)
-    | _ -> failwith "cannot move"
-
 let crates =
-    s[ 0 ].Split("\n")
-    |> Array.filter (fun row -> row.Contains("["))
-    |> Array.map parseCrates
-    |> Array.transpose
-    |> Array.map (fun r -> r |> Seq.filter (fun x -> x <> '#') |> Seq.toList)
+    cratesRaw.Split("\n")
+    |> Seq.filter (fun row -> row.Contains("["))
+    |> Seq.map parseCrates
+    |> Seq.transpose
+    |> Seq.map (fun r -> r |> Seq.filter (fun x -> x <> '#') |> Seq.toList)
 
 let moves =
-    s[1]
+    movesRaw
         .Replace("move ", "")
         .Replace(" from", "")
         .Replace(" to", "")
@@ -34,15 +27,50 @@ let moves =
         row.Split(" ")
         |> Array.map (fun d -> (d |> int) - 1))
 
-// Mutates crates
+// Part 1
+let makeMove source destination =
+    match source with
+    | x :: xs -> (xs, x :: destination)
+    | _ -> failwith "cannot move"
+
+let crates1 = crates |> Seq.toArray
+
 for move in moves do
     let times = move[0]
     let sourceIdx = move[1]
     let destIdx = move[2]
 
     for n in 0..times do
-        let (nextSource, nextDestination) = makeMove crates[sourceIdx] crates[destIdx]
-        crates[sourceIdx] <- nextSource
-        crates[destIdx] <- nextDestination
-    
-crates |> Array.map (fun col -> col |> List.head) |> System.String
+        let (nextSource, nextDestination) = makeMove crates1[sourceIdx] crates1[destIdx]
+        crates1[sourceIdx] <- nextSource
+        crates1[destIdx] <- nextDestination
+
+let part1 =
+    crates1
+    |> Array.map (fun col -> col |> List.head)
+    |> System.String
+
+// Part 2
+let makeMoves source destination times =
+    let toMove = source |> List.take times
+    let newSource = source |> List.removeManyAt 0 times
+    let newDestination = toMove @ destination
+    (newSource, newDestination)
+
+let crates2 = crates |> Seq.toArray
+
+for move in moves do
+    let times = move[0] + 1
+    let sourceIdx = move[1]
+    let destIdx = move[2]
+
+    let (nextSource, nextDestination) =
+        makeMoves crates2[sourceIdx] crates2[destIdx] times
+
+    crates2[sourceIdx] <- nextSource
+    crates2[destIdx] <- nextDestination
+
+let part2 =
+    crates2
+    |> Array.map (fun col -> col |> List.head)
+    |> System.String
