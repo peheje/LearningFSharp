800daafa0318d834d5ff0f97bc08430acca5c61a
Peter <peterhj@gmail.com>
2022-12-09 14:41:28 +0100

    Update Program.fs

--- AOC2022/06/Program.fs
+++ AOC2022/06/Program.fs
@@ -1,10 +1,12 @@
-﻿let path = "/Users/peterhelstrupjensen/repos/AOC2022_at_work/06/input"
+let path = "/Users/peterhelstrupjensen/repos/AOC2022_at_work/06/input"
 let rows = System.IO.File.ReadAllLines path
 
 let allUnique l =
     l |> Seq.distinct |> Seq.length = (l |> Seq.length)
 
 let firstUnique size row =
+    // Using Seq, should only evaluate the required amount of windows
+    // https://learn.microsoft.com/en-us/dotnet/fsharp/language-reference/sequences#searching-and-finding-elements
     (row |> Seq.windowed size |> Seq.findIndex allUnique) + size
 
 let firstUnique4 = firstUnique 4
