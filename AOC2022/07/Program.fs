8e94c30fea0cae3c9157564621e1dbccb2799664
peheje <peterhj@gmail.com>
2022-12-10 18:23:48 +0100

    lets go back

--- AOC2022/07/Program.fs
+++ AOC2022/07/Program.fs
@@ -1,4 +1,4 @@
-let path = "C:\Users\peter\Repos\LearningFSharp\AOC2022\07\input"
+let path = "/Users/phj/Code/F-Sharp-Advent-of-Code-2021/AOC2022/07/input"
 let rows =
     System.IO.File.ReadAllLines(path)
     |> Array.filter (fun row -> row.StartsWith("$ ls") |> not)
@@ -15,22 +15,14 @@ let changedToDirectoryName (row: string) = row.Split("$ cd ")[1]
 let directoryName (row: string) = row.Split("dir ")[1]
 let isGoBack (row: string) = row = "$ cd .."
 
-type Directory = { Name: string; mutable Parent: Directory option; mutable Files: int list; mutable Folders: Map<string, Directory> }
-
-let rec totalSize sum directory =
-
-    (directory.Folders |> Map.toList |> List.map snd)
-    |> List.reduce (fun a b ->
-        totalSize a b
-    ) 0
-
-
+type Directory = { Name: string; mutable Parent: Directory option; mutable Files: int list; mutable Folders: Directory list }
+    
 let rec buildGraph (rows: string list) (current: Directory): Directory =
     match rows with
     | row :: rest when row |> isDir ->
         let dirName = directoryName row
-        let dir = {Name=dirName; Parent=current.Parent; Files = []; Folders = Map.empty}
-        current.Folders <- (current.Folders |> Map.add dirName dir)
+        let dir = {Name=dirName; Parent=current.Parent; Files = []; Folders = List.empty}
+        current.Folders <- (dir :: current.Folders)
         buildGraph rest current
 
     | row :: rest when row |> isFile ->
@@ -40,7 +32,7 @@ let rec buildGraph (rows: string list) (current: Directory): Directory =
     
     | row :: rest when row |> isChangeDirectory ->
         let dirName = changedToDirectoryName row
-        let dir = (current.Folders |> Map.find dirName)
+        let dir = (current.Folders |> List.find (fun f -> f.Name = dirName))
         dir.Parent <- Some current
         buildGraph rest dir
 
@@ -52,9 +44,7 @@ let rec buildGraph (rows: string list) (current: Directory): Directory =
     | _ -> failwith "havent dont that part yet"
 
 
-let root = {Name="/"; Parent=None; Files = []; Folders = Map.empty}
+let root = {Name="/"; Parent=None; Files = []; Folders = List.empty}
 buildGraph rows root
 
 printfn "%A" root
-
-totalSize 0 root
