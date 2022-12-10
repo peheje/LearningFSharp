37abdc39c2d53ab479e1ee18a200e0e753d56071
PEHEJE <peterhj@gmail.com>
2022-12-10 16:26:30 +0100

    maybe correct path

--- AOC2022/07/Program.fs
+++ AOC2022/07/Program.fs
@@ -1,58 +1,42 @@
-﻿let path = "C:\Users\peter\Repos\LearningFSharp\AOC2022\07\input"
-let rows = System.IO.File.ReadAllLines(path) |> Array.toList
+let path = "C:\Users\peter\Repos\LearningFSharp\AOC2022\07\input"
+let rows =
+    System.IO.File.ReadAllLines(path)
+    |> Array.filter (fun row -> row.StartsWith("$ ls") |> not)
+    |> Array.skip 1
+    |> Array.toList
 
-let rowsFiltered =
-    rows
-    |> List.filter (fun row -> row.Contains("..") |> not)
-
-let isChangeDirectory (row: string) = row.StartsWith("$ cd")
+let isChangeDirectory (row: string) =
+    row.StartsWith("$ cd") && row.Contains("..") |> not
 
 let isFile (row: string) = System.Char.IsDigit(row[0])
-
 let isDir (row: string) = row.StartsWith("dir ")
-
 let fileSize (row: string) = row.Split(" ")[0] |> int
-
 let changedToDirectoryName (row: string) = row.Split("$ cd ")[1]
-
 let directoryName (row: string) = row.Split("dir ")[1]
 
-let rec group (rows: string list) dest =
-    match rows with
 
-    | row :: rest when row |> isChangeDirectory ->
-        let dir = row |> changedToDirectoryName
-        group rest ((dir, "0") :: dest)
+type Directory = { Name: string; Parent: string; Files: int list; Folders: Directory list }
+
+let rec buildGraph (rows: string list) (current: Directory): Directory =
+    match rows with
+    | row :: rest when row |> isDir ->
+        let dirName = directoryName row
+        let dir = {Name=dirName; Parent=current.Name; Files = []; Folders = []}
+        let current = { current with Folders = dir :: current.Folders }
+        buildGraph rest current
 
     | row :: rest when row |> isFile ->
-        let size = fileSize row
-        let currentDirectory = dest |> List.head |> fst
-        group rest ((currentDirectory, size |> string) :: dest)
+        let size = row |> fileSize
+        let current = { current with Files = size :: current.Files }
+        buildGraph rest current
+    
+    | row :: rest 
 
-    | row :: rest when row |> isDir ->
-        let directoryName = row |> directoryName
-        let currentDirectory = dest |> List.head |> fst
-        group rest ((currentDirectory, directoryName) :: dest)
+    | [] -> current
+    
+    | _ -> failwith "havent dont that part yet"
 
-    | [] -> dest
 
-    | _ :: rest -> group rest dest
+let g = buildGraph rows {Name="/"; Parent=""; Files = []; Folders = []}
 
-let structure = group rowsFiltered []
-
-let directFolderSizes =
-    structure
-    |> List.groupBy fst
-    |> List.map (fun (dir, items) -> (dir, items |> List.filter (snd >> isFile)))
-    |> List.map (fun (dir, sizes) -> (dir, sizes |> List.sumBy (snd >> int)))
-    |> Map.ofList
-
-let part1 =
-    structure
-    |> List.map (fun (dir, item) ->
-        if item |> isFile then
-            (dir, item |> int)
-        else
-            (dir, directFolderSizes |> Map.find item))
-    |> List.groupBy fst
-    |> List.map (fun (dir, sizes) -> (dir, sizes |> List.sumBy (snd >> int)))
+printfn "%A" g
