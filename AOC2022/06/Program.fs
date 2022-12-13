5095325be04d1e1eac43ee2750deb96fe8064f29
peheje <peterhj@gmail.com>
2022-12-13 22:59:50 +0100

    format

--- AOC2022/06/Program.fs
+++ AOC2022/06/Program.fs
@@ -5,23 +5,28 @@ let allUnique3 (xs: seq<'a>) =
     let seen = System.Collections.Generic.HashSet<'a>()
     use enumerator = xs.GetEnumerator()
     let mutable unique = true
+
     while unique && enumerator.MoveNext() do
         let current = enumerator.Current
+
         if seen.Contains current then
             unique <- false
         else
             seen.Add(current) |> ignore
+
     unique
-        
+
 let allUnique2 xs =
     let seen = System.Collections.Generic.HashSet<'a>()
-    xs |> Seq.tryFind (fun x ->
-        if seen.Contains(x) then
+
+    xs
+    |> Seq.tryFind (fun x ->
+        if seen.Contains x then
             true
         else
-            seen.Add(x) |> ignore
-            false
-    ) |> Option.isNone
+            seen.Add x |> ignore
+            false)
+    |> Option.isNone
 
 let allUnique1 l =
     l |> Seq.distinct |> Seq.length = (l |> Seq.length)
@@ -29,7 +34,10 @@ let allUnique1 l =
 let firstUnique size row =
     // Using Seq will only evaluate the required amount of windows
     // https://learn.microsoft.com/en-us/dotnet/fsharp/language-reference/sequences#searching-and-finding-elements
-    (row |> Seq.windowed size |> Seq.findIndex allUnique3) + size
+    (row
+     |> Seq.windowed size
+     |> Seq.findIndex allUnique3)
+    + size
 
 let firstUnique4 = firstUnique 4
 let firstUnique14 = firstUnique 14
