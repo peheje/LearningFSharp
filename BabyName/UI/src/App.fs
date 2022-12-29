91e6f53b03706ca32c8683a23b99ed867cb72fd8
PEHEJE <peterhj@gmail.com>
2022-12-29 20:35:25 +0100

    cleanup

--- BabyName/UI/src/App.fs
+++ BabyName/UI/src/App.fs
@@ -16,21 +16,20 @@ let setLocalStorage key value =
 let split (separator: char) (source: string) =
     source.Split separator
 
+let join (separator: char) (source: string array) =
+    String.Join(separator, source)
+
 let liked = document.querySelector("#liked") :?> Browser.Types.HTMLTextAreaElement
 let nameText = document.querySelector("#name") :?> Browser.Types.HTMLTextAreaElement
 let yes = document.querySelector("#yes") :?> Browser.Types.HTMLButtonElement
 let no = document.querySelector("#no") :?> Browser.Types.HTMLButtonElement
 let copy = document.querySelector("#copy") :?> Browser.Types.HTMLButtonElement
-let username = document.querySelector("#username") :?> Browser.Types.HTMLInputElement
 
 let usernameIsValid username =
     username |> Seq.length > 4
 
-let toBase64 (input: string) =
-    Text.Encoding.UTF8.GetBytes(input) |> Convert.ToBase64String
-
 let appendLiked message =
-    liked.textContent <- sprintf "\n%s" message + liked.textContent
+    liked.textContent <- message + "\n" + liked.textContent
 
 let capitalizeName (name: string) =
     let nameSeparator = if name.Contains('-') then '-' else ' '
@@ -38,11 +37,12 @@ let capitalizeName (name: string) =
     let capitalizedParts = parts |> Array.map (fun part ->
         part.Substring(0, 1).ToUpper() + part.Substring(1).ToLower()
     )
-    String.Join(nameSeparator, capitalizedParts)
+    capitalizedParts |> join nameSeparator
 
 let nameIterator () =
     let liked = getLocalStorageOrEmpty "liked" |> split ';'
-    appendLiked (String.Join('\n', liked))
+    liked |> Array.rev |> join '\n' |> appendLiked 
+    
     let disliked = getLocalStorageOrEmpty "disliked" |> split ';'
     let nonProcessedNames =
         names
@@ -74,21 +74,6 @@ let addToLiked name =
     appendToLocalStorage "liked" name
     appendLiked name
 
-let setInputValue value (input: Browser.Types.HTMLInputElement) =
-    input.value <- value
-    input.dispatchEvent(Browser.Event.Event.Create("input")) |> ignore
-
-username.oninput <- fun _ ->
-    if username.value |> usernameIsValid then
-        setLocalStorage "username" username.value
-        yes.disabled <- false
-        no.disabled <- false
-    else
-        yes.disabled <- true
-        no.disabled <- true
-
-setInputValue (getLocalStorageOrEmpty "username") username
-
 askNext ()
 
 [<Emit("navigator.clipboard.writeText($0)")>]
@@ -97,7 +82,7 @@ let private writeToClipboard _text : JS.Promise<unit> = jsNative
 copy.onclick <- fun _ ->
     async {
         try
-            do! liked.textContent |> toBase64 |> writeToClipboard |> Async.AwaitPromise 
+            do! liked.textContent |> writeToClipboard |> Async.AwaitPromise 
         with ex ->
             printfn "Promise rejected %s" ex.Message
     } |> Async.StartImmediate
