be146a645db92ca01f459162f01f1a29eeae0a1a
peheje <peterhj@gmail.com>
2022-12-30 20:56:57 +0100

    reformat and then cleanup from here

--- BabyName/UI/src/App.fs
+++ BabyName/UI/src/App.fs
@@ -1,6 +1,7 @@
 module App
 
 open Browser.Dom
+open Browser.Types
 open System
 open Data
 open Fable.Core
@@ -11,25 +12,24 @@ let getLocalStorageOrEmpty key =
     | x -> x
 
 let setLocalStorage key value =
-    Browser.WebStorage.localStorage.setItem(key, value)
+    Browser.WebStorage.localStorage.setItem (key, value)
 
-let split (separator: char) (source: string) =
-    source.Split separator
+let split (separator: char) (source: string) = source.Split separator
+let join (separator: char) (source: string array) = String.Join(separator, source)
+let id id = document.getElementById (id)
 
-let join (separator: char) (source: string array) =
-    String.Join(separator, source)
-
-let liked = document.querySelector("#liked") :?> Browser.Types.HTMLTextAreaElement
-let nameText = document.querySelector("#name") :?> Browser.Types.HTMLTextAreaElement
-let yes = document.querySelector("#yes") :?> Browser.Types.HTMLButtonElement
-let no = document.querySelector("#no") :?> Browser.Types.HTMLButtonElement
-let copy = document.querySelector("#copy") :?> Browser.Types.HTMLButtonElement
+let liked = id "liked" :?> HTMLTextAreaElement
+let nameText = id "name" :?> HTMLTextAreaElement
+let yes = id "yes" :?> HTMLButtonElement
+let no = id "no" :?> HTMLButtonElement
+let copy = id "copy" :?> HTMLButtonElement
 
 let appendLiked message =
     liked.textContent <- message + "\n" + liked.textContent
 
 let capitalizeName (name: string) =
     let nameSeparator = if name.Contains('-') then '-' else ' '
+
     name
     |> split nameSeparator
     |> Array.map (fun part -> part.Substring(0, 1).ToUpper() + part.Substring(1).ToLower())
@@ -38,34 +38,38 @@ let capitalizeName (name: string) =
 let nameIterator () =
     let liked = getLocalStorageOrEmpty "liked" |> split ';'
     liked |> Array.rev |> join '\n' |> appendLiked
-    
+
     let disliked = getLocalStorageOrEmpty "disliked" |> split ';'
+
     let nonProcessedNames =
-        names
-        |> Array.map capitalizeName
-        |> Array.except liked
-        |> Array.except disliked
+        names |> Array.map capitalizeName |> Array.except liked |> Array.except disliked
+
     let mutable index = -1
+
     let currentName () =
         if index = -1 then "" else nonProcessedNames[index]
+
     let nextName () =
         index <- index + 1
         currentName ()
+
     (nextName, currentName)
 
-let (nextName, currentName) = nameIterator()
+let (nextName, currentName) = nameIterator ()
 
 let askNext () =
     nameText.textContent <- sprintf "Do you like %s?" (nextName ())
 
 let appendToLocalStorage key name =
     let current = getLocalStorageOrEmpty key
+
     if current = "" then
         setLocalStorage key name
     else
         setLocalStorage key (current + ";" + name)
 
 let addToDisliked name = appendToLocalStorage "disliked" name
+
 let addToLiked name =
     appendToLocalStorage "liked" name
     appendLiked name
@@ -75,21 +79,24 @@ askNext ()
 [<Emit("navigator.clipboard.writeText($0)")>]
 let private writeToClipboard _text : JS.Promise<unit> = jsNative
 
-copy.onclick <- fun _ ->
-    async {
-        try
-            do! liked.textContent |> writeToClipboard |> Async.AwaitPromise 
-        with ex ->
-            printfn "Promise rejected %s" ex.Message
-    } |> Async.StartImmediate
-    
-yes.onclick <- fun _ ->
-    let name = currentName()
-    addToLiked name
-    askNext ()
+copy.onclick <-
+    fun _ ->
+        async {
+            try
+                do! liked.textContent |> writeToClipboard |> Async.AwaitPromise
+            with ex ->
+                printfn "Promise rejected %s" ex.Message
+        }
+        |> Async.StartImmediate
 
-no.onclick <- fun _ ->
-    let name = currentName()
-    addToDisliked name
-    askNext ()
+yes.onclick <-
+    fun _ ->
+        let name = currentName ()
+        addToLiked name
+        askNext ()
 
+no.onclick <-
+    fun _ ->
+        let name = currentName ()
+        addToDisliked name
+        askNext ()
