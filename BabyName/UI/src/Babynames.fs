6a39b060b2e3e4f5727ed6191b30fa53efadea80
PEHEJE <peterhj@gmail.com>
2023-02-18 12:26:07 +0100

    make baby names its own module

---
+++ BabyName/UI/src/Babynames.fs
@@ -0,0 +1,61 @@
+module BabyNames
+
+open Browser.Types
+open Data
+open Html
+open Browser
+
+let private initGenderSelector () =
+    let girl = id "girl" :?> HTMLInputElement
+    let boy = id "boy" :?> HTMLInputElement
+
+    [|girl; boy|] |> Array.iter (fun el -> el |> onClick (fun _ ->
+        setLocalStorage "gender" el.value
+        window.location.reload ()))
+
+    if getLocalStorageOrEmpty "gender" = "boy" then
+        boy.checked <- true
+        boyNames
+    else
+        girl.checked <- true
+        girlNames
+
+let initBabyNames () =
+    let liked = getLocalStorageOrEmpty "liked" |> split ';'
+    let disliked = getLocalStorageOrEmpty "disliked" |> split ';'
+    let nameElement = id "name"
+    let likedElement = id "liked" :?> HTMLTextAreaElement
+    let mutable index = -1
+
+    let unprocessedNames =
+        initGenderSelector () |> Array.except liked |> Array.except disliked
+
+    let appendLiked message =
+        likedElement.textContent <- message + "\n" + likedElement.textContent
+
+    let like () =
+        unprocessedNames[index] |> appendToLocalStorageList "liked"
+        unprocessedNames[index] |> appendLiked
+
+    let dislike () =
+        unprocessedNames[index] |> appendToLocalStorageList "disliked"
+
+    let askNext () =
+        index <- index + 1
+        nameElement.textContent <- sprintf "Do you like %s?" (unprocessedNames[index])
+
+    let confirmClear () =
+        let prompt = "delete all liked and disliked names"
+        if window.prompt $"Type '{prompt}' to continue." = prompt then
+            localStorage.clear()
+            window.location.reload ()
+
+    liked |> Array.rev |> join '\n' |> appendLiked
+    id "yes" |> onClick (like >> askNext)
+    id "no" |> onClick (dislike >> askNext)
+    id "clear" |> onClick confirmClear
+    id "copy" |> onClick (fun _ -> toClipboard likedElement.textContent)
+
+    askNext()
+
+initBabyNames ()
