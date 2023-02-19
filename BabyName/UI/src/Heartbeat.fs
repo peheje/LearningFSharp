fc5edac98890ee516bcbb8db8f0c46b0d2bd4acb
peheje <peterhj@gmail.com>
2023-02-19 14:26:55 +0100

    wip

--- BabyName/UI/src/Heartbeat.fs
+++ BabyName/UI/src/Heartbeat.fs
@@ -8,9 +8,14 @@ open Browser.Types
 
 let initHeartbeat () =
     let mutable pressed = false
-    let beats = List<DateTimeOffset>()
+    let beats = List<int64>()
     let beatElement = fromId "beat"
     let heartbeatElement = fromId "heartbeat"
+
+    let test () =
+        window.setInterval((fun _ -> 
+            document.dispatchEvent(Event.Create("KeyboardEvent", ))
+        ))
 
     let showBeatIndicator () =
         beatElement.removeAttribute("hidden")
@@ -18,27 +23,20 @@ let initHeartbeat () =
             beatElement.setAttribute("hidden", "hidden")
         ), 200) |> ignore
 
-    let keepOnlyNewestNBeats n =
+    let limitBeats n =
         if beats |> Seq.length = n then
             beats.RemoveAt 0
 
     let beatKeyNotPressed _ = pressed <- false
 
-    let beatKeyPressed (event: Event) =
-        let keyboardEvent = event :?> KeyboardEvent
-        let key = keyboardEvent.key
-        printfn "%A" key
-
+    let beat key =
         if pressed || key <> " " then ()
         else
 
         pressed <- true
-
         showBeatIndicator ()
-        
-        keepOnlyNewestNBeats 6
-
-        beats.Add DateTimeOffset.UtcNow
+        limitBeats 6
+        beats.Add (DateTimeOffset.UtcNow.ToUnixTimeMilliseconds ())
 
         if beats |> Seq.length < 2 then ()
         else
@@ -46,15 +44,17 @@ let initHeartbeat () =
         let averageTimeBetweenBeatMs =
             beats
             |> Seq.pairwise
-            |> Seq.map (fun (first, second) ->
-                let delta = second.ToUnixTimeMilliseconds () - first.ToUnixTimeMilliseconds ()
-                delta |> float
-            )
+            |> Seq.map (fun (a, b) -> b - a |> float)
             |> Seq.average
         
         let bpm = 60000.0 / averageTimeBetweenBeatMs
         heartbeatElement.innerHTML <- bpm.ToString("#.##") + " bpm"
 
+    let beatKeyPressed (event: Event) =
+        let keyboardEvent = event :?> KeyboardEvent
+        beat keyboardEvent.key
+
     document.addEventListener("keyup", beatKeyNotPressed)
     document.addEventListener("mouseup", beatKeyNotPressed)
     document.addEventListener("keydown", beatKeyPressed)
+    (inputFromId "heartbeat-btn") |> onClick (fun _ -> beat " ")
