1fd3142c805719ddf49d0fc278457fa1c6b36911
peheje <peterhj@gmail.com>
2023-03-20 17:43:53 +0100

    change logic

--- BabyName/UI/src/Days.fs
+++ BabyName/UI/src/Days.fs
@@ -4,13 +4,7 @@ open System
 open Html
 
 let private isWeekend (time: DateTime) =
-    match time.DayOfWeek with
-    | DayOfWeek.Saturday | DayOfWeek.Sunday -> true
-    | _ -> false
-
-let private counter () =
-    let mutable c = 0
-    (fun () -> c <- c + 1), (fun () -> c)
+    time.DayOfWeek = DayOfWeek.Saturday || time.DayOfWeek = DayOfWeek.Sunday
 
 let private pluralize word count =
     if count = 1 then word
@@ -18,7 +12,6 @@ let private pluralize word count =
 
 let private formatDays totalDays =
     let weeks, remainingDays = Math.DivRem(totalDays, 7)
-    
     let daysText = sprintf "%i %s" totalDays (pluralize "day" totalDays)
     let weeksText = sprintf "(%i %s" weeks (pluralize "week" weeks)
     let remainingWeekDaysText = sprintf " and %i %s)" remainingDays (pluralize "day" remainingDays)
@@ -38,20 +31,25 @@ let initDays () =
     let endDayInput = (inputFromId "end-day")
     endDayInput.valueAsDate <- DateTime.Now
 
+    let rec collectDays (cursor: DateTime) stop out =
+        if cursor < stop then
+            collectDays (cursor.AddDays(1)) stop (cursor :: out)
+        else
+            out
+
     let calculate () =
-        let addDay, days = counter ()
-        let addWeekendDay, weekendDays = counter ()
+        let start = startDayInput.valueAsDate
+        let stop = endDayInput.valueAsDate
+        let days = collectDays start stop List.empty
+        
+        let daysCount = days |> List.length
+        let weekendCount = days |> List.filter isWeekend |> List.length
+        
+        let daysText = formatDays daysCount
+        let weekendText = formatDays weekendCount
 
-        let mutable cursor = startDayInput.valueAsDate
-        while cursor < endDayInput.valueAsDate do
-            addDay ()
-            if isWeekend cursor then addWeekendDay()
-            cursor <- cursor.AddDays(1)
-
-        (fromId "total-days").textContent <- (days () |> formatDays)
-
-        let pluralizedWeekend = pluralize "day" (weekendDays ())
-        (fromId "weekend-days").textContent <- ((weekendDays () |> string) + " weekend " + pluralizedWeekend)
+        (fromId "total-days").textContent <- daysText
+        (fromId "weekend-days").textContent <- weekendText
 
     startDayInput |> onChange calculate
     endDayInput |> onChange calculate
