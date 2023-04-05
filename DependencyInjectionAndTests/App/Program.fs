42ec420df5213e298fa969d34949dc00b8631169
peheje <peterhj@gmail.com>
2023-04-05 22:36:18 +0200

    intermediary step

--- DependencyInjectionAndTests/App/Program.fs
+++ DependencyInjectionAndTests/App/Program.fs
@@ -1,12 +1,26 @@
 ﻿module App
 
+// I don't want to make a SQLite or Postgres or any real db, it's not necessary, instead this acts as something you can't really mock out, somewhere you have to call the selecting/saving, could be a SQL-client nuget package code
+module NastyDatabaseDifficultToMockOut =
+    let select id =
+        "some dummy data"
+    let insert id data =
+        // don't save, just return unit
+        ()
+
+module CacheRepository =
+    let get id =
+        NastyDatabaseDifficultToMockOut.select id
+
+    let save id data =
+        NastyDatabaseDifficultToMockOut.insert id data
+
 module Greeter =
     let greet getHour =
         let hour = getHour ()
         if hour > 4 && hour < 10 then "Goodmorning" else "Hi!"
 
-// Somewhere in production code, you don't want to have to say
-// to use UtcNow everytime to get hour, so you create a helper
+// Somewhere in production code, you don't want to have to say to use UtcNow everytime to get hour, so you create a helper
 let greeterRealTime () =
     Greeter.greet (fun () -> System.DateTimeOffset.UtcNow.Hour)
 
