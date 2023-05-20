17ef44d19030daada85a640b14ac9f075f29c779
PEHEJE <peterhj@gmail.com>
2023-05-20 22:22:42 +0200

    best so far

--- BabyName/UI/src/Typing.fs
+++ BabyName/UI/src/Typing.fs
@@ -10,6 +10,7 @@ open Browser.Types
 let private input = inputFromId "input"
 let private timer = fromId "timer"
 let private target = "the quick brown fox jumps over the lazy dog"
+let private storageKey = "best-typing-time"
 
 let mutable private startTime: DateTime option = None
 let mutable private timerId = -1
@@ -17,6 +18,7 @@ let mutable private timerId = -1
 let private printElapsedTime () =
     let duration = (DateTime.UtcNow - startTime.Value)
     timer.innerText <- duration.TotalSeconds |> string
+    duration.TotalMilliseconds
 
 let private onKey (e: KeyboardEvent) =
     if e.key = "F5" then
@@ -24,7 +26,7 @@ let private onKey (e: KeyboardEvent) =
     else
         if startTime |> Option.isNone then
             startTime <- Some DateTime.UtcNow
-            timerId <- setInterval printElapsedTime 100
+            timerId <- setInterval (fun _ -> printElapsedTime () |> ignore) 100
 
         if target.StartsWith input.value then
             document.body?style?backgroundColor <- "green"
@@ -32,10 +34,23 @@ let private onKey (e: KeyboardEvent) =
             if target = input.value then
                 document.body?style?backgroundColor <- "gold"
                 clearInterval timerId
-                printElapsedTime ()
+                let elapsedMs = printElapsedTime ()
+
+                match getLocalStorageOrEmpty storageKey with
+                | "" -> setLocalStorage storageKey (string elapsedMs)
+                | previousBestMs ->
+                    if (previousBestMs |> float) > elapsedMs then
+                        setLocalStorage storageKey (string elapsedMs)
+
         else
             document.body?style?backgroundColor <- "red"
 
 let initTyping () =
+    match getLocalStorageOrEmpty storageKey with
+    | "" -> ()
+    | ms ->
+        let bestElement = (fromId "best")
+        bestElement.textContent <- sprintf "Best so far is %sms" ms
+                
     input.focus ()
     input |> onKeyUp onKey
