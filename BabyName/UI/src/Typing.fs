8dcb6c5aaa28324cc116da0b136faf2fe2d18cdc
PEHEJE <peterhj@gmail.com>
2023-05-20 21:13:21 +0200

    fantomas format

--- BabyName/UI/src/Typing.fs
+++ BabyName/UI/src/Typing.fs
@@ -19,21 +19,23 @@ let private printElapsedTime () =
     timer.innerText <- duration.TotalSeconds |> string
 
 let private onKey (e: KeyboardEvent) =
-    if e.key = "F5" then ()
+    if e.key = "F5" then
+        ()
     else
-    if startTime |> Option.isNone then
-        startTime <- Some DateTime.UtcNow
-        timerId <- setInterval printElapsedTime 100
+        if startTime |> Option.isNone then
+            startTime <- Some DateTime.UtcNow
+            timerId <- setInterval printElapsedTime 100
 
-    if target.StartsWith input.value then
-        document.body?style?backgroundColor <- "green"
-        if target = input.value then
-            document.body?style?backgroundColor <- "gold"
-            clearInterval timerId
-            printElapsedTime ()
-    else
-        document.body?style?backgroundColor <- "red"
-    
+        if target.StartsWith input.value then
+            document.body?style?backgroundColor <- "green"
+
+            if target = input.value then
+                document.body?style?backgroundColor <- "gold"
+                clearInterval timerId
+                printElapsedTime ()
+        else
+            document.body?style?backgroundColor <- "red"
+
 let initTyping () =
-    input.focus()
+    input.focus ()
     input |> onKeyUp onKey
