be31304b73a242b2aeba7d3759808cbfbade6a99
PEHEJE <peterhj@gmail.com>
2023-05-29 23:36:49 +0200

    use benchmarks to verify that using seq and scan with takeWhile is faster and uses less memory, as it can stop computation when view is blocked

--- AOC2022/08/Program.fs
+++ AOC2022/08/Program.fs
@@ -1,44 +1,61 @@
-﻿let path = "C:\Users\peter\Repos\LearningFSharp\AOC2022\08\data.txt"
+﻿open BenchmarkDotNet.Attributes
+open BenchmarkDotNet.Running
 
-let xxs =
-    System.IO.File.ReadAllLines path
-    |> Array.map Array.ofSeq
-    |> Array.map (Array.map (System.Char.GetNumericValue >> int))
+[<MemoryDiagnoser>]
+type MyBenchmark() =
+    
+    let mutable xxs: int array array = Array.empty
+    
+    let moveUp x y =
+        seq { for i in y .. -1 .. 0 -> xxs[i][x] }
 
-let moveUp x y =
-    [| for i in y .. -1 .. 0 -> xxs[i][x] |]
+    let moveDown x y =
+        seq { for i in y .. xxs.Length - 1 -> xxs[i][x] }
 
-let moveDown x y =
-    [| for i in y .. xxs.Length - 1 -> xxs[i][x] |]
+    let moveLeft x y =
+        seq { for i in x .. -1 .. 0 -> xxs[y][i] }
 
-let moveLeft x y =
-    [| for i in x .. -1 .. 0 -> xxs[y][i] |]
+    let moveRight x y =
+        seq { for i in x .. xxs[0].Length - 1 -> xxs[y][i] }
 
-let moveRight x y =
-    [| for i in x .. xxs[0].Length - 1 -> xxs[y][i] |]
+    let scenic treesInDirection x y =
+        let origin = xxs[y][x]
 
-let scenic treesInDirection x y =
-    let origin = xxs[y][x]
+        treesInDirection x y
+        |> Seq.skip 1
+        |> Seq.scan
+            (fun (stop, count) tree ->
+                match (stop, tree) with
+                | true, _ -> (true, false)
+                | _, v when v >= origin -> (true, true)
+                | _ -> (false, true))
+            (false, false)
+        |> Seq.skip 1
+        |> Seq.takeWhile snd
+        |> Seq.length
 
-    treesInDirection x y
-    |> Array.skip 1
-    |> Array.fold
-        (fun (stop, count) tree ->
-            match (stop, tree) with
-            | true, _ -> (true, count)
-            | _, v when v >= origin -> (true, count + 1)
-            | _ -> (false, count + 1))
-        (false, 0)
-    |> snd
+    let score x y =
+        (scenic moveUp x y)
+        * (scenic moveDown x y)
+        * (scenic moveLeft x y)
+        * (scenic moveRight x y)
+    
+    [<GlobalSetup>]
+    member this.GlobalSetup() =
+        let path = "C:\Users\peter\Repos\LearningFSharp\AOC2022\08\data.txt"
 
-let score x y =
-    (scenic moveUp x y)
-    * (scenic moveDown x y)
-    * (scenic moveLeft x y)
-    * (scenic moveRight x y)
+        xxs <-
+            System.IO.File.ReadAllLines path
+            |> Array.map Array.ofSeq
+            |> Array.map (Array.map (System.Char.GetNumericValue >> int))
+    
+    [<Benchmark>]
+    member this.BenchmarkMethod() = 
+        let best =
+            xxs
+            |> Array.mapi (fun y vs -> vs |> Array.mapi (fun x _ -> score x y))
+            |> Array.collect id
+            |> Array.max
+        printfn "%i" best
 
-let scores =
-    xxs
-    |> Array.mapi (fun y vs -> vs |> Array.mapi (fun x _ -> score x y))
-    |> Array.collect id
-    |> Array.max
+BenchmarkRunner.Run<MyBenchmark>() |> ignore
