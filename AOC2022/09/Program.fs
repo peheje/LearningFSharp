cf78a7e34b95dcd694e5e309ac8423f176ade71e
PEHEJE <peterhj@gmail.com>
2023-06-12 23:27:33 +0200

    iterative solution

--- AOC2022/09/Program.fs
+++ AOC2022/09/Program.fs
@@ -9,40 +9,54 @@ let moves =
         let steps = int s[1]
         Array.create steps dir)
     |> Array.collect id
-    |> Array.toList
 
-let shouldMoveTail head tail =
-    let (hx, hy) = head
-    let (tx, ty) = tail
-    let deltax = abs (hx - tx)
-    let deltay = abs (hy - ty)
-    deltax > 1 || deltay > 1
+let printStep rope move =
+    let maxx = rope |> Array.map fst |> Array.max
+    let minx = rope |> Array.map fst |> Array.min
+    let maxy = rope |> Array.map snd |> Array.max
+    let miny = rope |> Array.map snd |> Array.min
 
-let newTailPosition head previousHead tail =
-    if shouldMoveTail head tail then
-        previousHead
-    else
-        tail
-
-let rec move tailBeen tail head moves =
-    let x, y = head
-
-    match moves with
-    | [] -> tailBeen
-    | nextMove :: remainingMoves ->
-        let newHead =
-            match nextMove with
-            | "R" -> (x + 1, y)
-            | "L" -> (x - 1, y)
-            | "U" -> (x, y + 1)
-            | "D" -> (x, y - 1)
-            | _ -> failwith "unknown move"
-
-        let newTail = newTailPosition newHead head tail
-        move (newTail :: tailBeen) newTail newHead remainingMoves
+    printfn "move %s" move
+    let mincord, maxcord = -6, 6
+    for y in mincord..maxcord do
+        for x in mincord..maxcord do
+            match rope |> Array.tryFindIndex (fun spot -> spot = (x, y)) with
+            | Some v -> printf "%i " v
+            | None -> printf ". "
+        printfn ""
+    printfn ""
+    printfn ""
 
 
-move List.empty (0, 0) (0, 0) moves
-|> List.distinct
-|> List.length
-|> printfn "%A"
+let isDetached head tail =
+    let hx, hy = head
+    let tx, ty = tail
+    (abs (hx-tx) > 1) || (abs (hy-ty) > 1)
+
+let rope = Array.init 2 (fun _ -> (0,0))
+
+printStep rope "initial state"
+let visited = System.Collections.Generic.HashSet<(int*int)>()
+for move in moves do
+    let hx, hy = rope[0]
+    match move with
+    | "R" ->
+        rope[0] <- hx + 1, hy
+        if isDetached rope[0] rope[1] then
+            rope[1] <- hx, hy
+    | "L" ->
+        rope[0] <- hx - 1, hy
+        if isDetached rope[0] rope[1] then
+            rope[1] <- hx, hy
+    | "U" ->
+        rope[0] <- hx, hy - 1
+        if isDetached rope[0] rope[1] then
+            rope[1] <- hx, hy
+    | "D" ->
+        rope[0] <- hx, hy + 1
+        if isDetached rope[0] rope[1] then
+            rope[1] <- hx, hy
+    visited.Add rope[1] |> ignore
+    //printStep rope move
+
+printfn "%i" visited.Count
