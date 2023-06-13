f03219203acaf15402dba36f66e28cab8b8dfa0d
peheje <peterhj@gmail.com>
2023-06-13 21:08:27 +0200

    format

--- AOC2022/09/Program.fs
+++ AOC2022/09/Program.fs
@@ -12,12 +12,15 @@ let moves =
 let printStep rope move =
     printfn "move %s" move
     let mincord, maxcord = -6, 6
+
     for y in mincord..maxcord do
         for x in mincord..maxcord do
             match rope |> Array.tryFindIndex (fun spot -> spot = (x, y)) with
             | Some v -> printf "%i " v
             | None -> printf ". "
+
         printfn ""
+
     printfn ""
     printfn ""
 
@@ -35,11 +38,11 @@ let catchupMove head tail =
     if abs xdif + abs ydif < 3 then
         // catch up in L, R, D, U or not
         match xdif, ydif with
-        | x, _ when x > 1  -> moveRight tail
+        | x, _ when x > 1 -> moveRight tail
         | x, _ when x < -1 -> moveLeft tail
         | _, y when y < -1 -> moveUp tail
-        | _, y when y > 1  -> moveDown tail
-        | _, _             -> tail
+        | _, y when y > 1 -> moveDown tail
+        | _, _ -> tail
     else
         // catch up diagonally
         let upOrDown = if ydif < 0 then moveUp else moveDown
@@ -48,17 +51,22 @@ let catchupMove head tail =
 
 
 seq {
-    let rope = Array.init 10 (fun _ -> (0,0))
+    let rope = Array.init 10 (fun _ -> (0, 0))
+
     for move in moves do
         let hx, hy = rope[0]
+
         match move with
         | "R" -> rope[0] <- hx + 1, hy
         | "L" -> rope[0] <- hx - 1, hy
         | "U" -> rope[0] <- hx, hy - 1
         | "D" -> rope[0] <- hx, hy + 1
 
-        for i in 0..rope.Length-2 do
-            rope[i+1] <- catchupMove rope[i] rope[i+1]
+        for i in 0 .. rope.Length - 2 do
+            rope[i + 1] <- catchupMove rope[i] rope[i + 1]
 
         rope[9]
-} |> Seq.distinct |> Seq.length |> printfn "%i"
+}
+|> Seq.distinct
+|> Seq.length
+|> printfn "%i"
