d9876af07a6886a8d30571c95ee3fcb9d8649d86
peheje <peterhj@gmail.com>
2023-06-19 00:15:35 +0200

    part 1, need prettyfication

--- AOC2022/kotlin/aoc2022/src/main/kotlin/Main.kt
+++ AOC2022/kotlin/aoc2022/src/main/kotlin/Main.kt
@@ -1,31 +1,81 @@
+import java.io.File
 import java.util.PriorityQueue
 
+@JvmInline
+value class EdgeId(val v: Pair<Int, Int>)
+
 fun main() {
-    val graph = HashMap<String, List<Pair<String, Int>>>()
-    val distances = hashMapOf<String, Int>()
-    val previous = HashMap<String, String?>()
 
-    val source = "a"
-    graph["a"] = listOf("c" to 5, "b" to 2)
-    graph["b"] = listOf("a" to 2, "c" to 3, "d" to 3)
-    graph["c"] = listOf("a" to 5, "b" to 3, "e" to 2)
-    graph["d"] = listOf("a" to 7, "b" to 3, "e" to 1)
-    graph["e"] = listOf("c" to 2, "d" to 1)
+    val path = "/Users/phj/Code/F-Sharp-Advent-of-Code-2021/AOC2022/kotlin/aoc2022/src/main/kotlin/input12.txt"
+    val rows = File(path).readLines()
 
-    val queue = PriorityQueue<Pair<String, Int>>(compareBy { it.second })
+    var destination: EdgeId? = null
+    var source: EdgeId? = null
+
+    val map = rows.mapIndexed { y, row ->
+        row.toList().mapIndexed { x, char ->
+            when (char) {
+                'S' -> {
+                    source = EdgeId(Pair(x, y))
+                    'a'.code
+                }
+                'E' -> {
+                    destination = EdgeId(Pair(x, y))
+                    'z'.code
+                }
+                else -> char.code
+            } - 97
+        }
+    }
+    println(map.toString())
+
+    fun neighborIndices(x: Int, y: Int): List<Pair<Int, Int>> {
+        val indices = mutableListOf<Pair<Int, Int>>()
+        if (x > 0) {
+            indices.add(Pair(x - 1, y))
+        }
+        if (x < map.first().lastIndex) {
+            indices.add(Pair(x + 1, y))
+        }
+        if (y > 0) {
+            indices.add(Pair(x, y - 1))
+        }
+        if (y < map.lastIndex) {
+            indices.add(Pair(x, y + 1))
+        }
+        return indices
+    }
+
     val infinite = 1000
+
+    val graph = HashMap<EdgeId, List<Pair<EdgeId, Int>>>()
+    map.forEachIndexed { y, row ->
+        row.forEachIndexed { x, value ->
+            val indices = neighborIndices(x, y).map { neighbor ->
+                val (nx, ny) = neighbor
+                val difference = map[ny][nx] - value
+                val cost = if (difference > 1) infinite else 1
+                Pair(EdgeId(neighbor), cost)
+            }
+            graph[EdgeId(Pair(x, y))] = indices
+        }
+    }
+
+    val distances = hashMapOf<EdgeId, Int>()
+    val previous = HashMap<EdgeId, EdgeId?>()
+    val queue = PriorityQueue<Pair<EdgeId, Int>>(compareBy { it.second })
 
     for (v in graph.keys) {
         distances[v] = infinite
         previous[v] = null
         queue.add(v to infinite)
     }
-    distances[source] = 0
+    distances[source!!] = 0
 
     while (queue.isNotEmpty()) {
         val (edge, _) = queue.poll()
 
-        graph[edge]?.forEach { (neighbor, weight) ->
+        graph.getValue(edge).forEach { (neighbor, weight) ->
             val totalDistance = distances.getValue(edge) + weight
             if (totalDistance < distances.getValue(neighbor)) {
                 distances[neighbor] = totalDistance
@@ -35,11 +85,20 @@ fun main() {
         }
     }
 
-    assert(previous["e"] == "d")
-    assert(previous["d"] == "b")
-    assert(previous["b"] == "a")
+    var cursor = destination
+    val shortestPath = mutableListOf<EdgeId?>()
+    do {
+        val step = previous.getValue(cursor!!) ?: continue
+        shortestPath.add(step)
+        cursor = step
 
-    println("Done")
+    } while (cursor != source)
+
+    shortestPath.reversed().forEach {
+        println(it)
+    }
+
+    println("Done with shortest path ${shortestPath.size}")
 }
 
 
