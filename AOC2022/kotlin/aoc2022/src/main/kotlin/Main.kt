14c8d266152088dba466ab0413b924d75e760d24
PEHEJE <peterhj@gmail.com>
2023-06-19 19:04:40 +0200

    lil cleanup

--- AOC2022/kotlin/aoc2022/src/main/kotlin/Main.kt
+++ AOC2022/kotlin/aoc2022/src/main/kotlin/Main.kt
@@ -1,16 +1,14 @@
 import java.io.File
 import java.util.PriorityQueue
 
-@JvmInline
-value class EdgeId(val v: Pair<Int, Int>)
+data class EdgeId(val v: Pair<Int, Int>)
 
 fun main() {
-
-    val path = "/Users/phj/Code/F-Sharp-Advent-of-Code-2021/AOC2022/kotlin/aoc2022/src/main/kotlin/input12.txt"
+    val path = "C:\\Users\\peter\\Repos\\LearningFSharp\\AOC2022\\kotlin\\aoc2022\\src\\main\\kotlin\\input12.txt"
     val rows = File(path).readLines()
 
-    var destination: EdgeId? = null
-    var source: EdgeId? = null
+    lateinit var destination: EdgeId
+    lateinit var source: EdgeId
 
     val map = rows.mapIndexed { y, row ->
         row.toList().mapIndexed { x, char ->
@@ -19,30 +17,27 @@ fun main() {
                     source = EdgeId(Pair(x, y))
                     'a'.code
                 }
+
                 'E' -> {
                     destination = EdgeId(Pair(x, y))
                     'z'.code
                 }
+
                 else -> char.code
             } - 97
         }
     }
-    println(map.toString())
 
     fun neighborIndices(x: Int, y: Int): List<Pair<Int, Int>> {
         val indices = mutableListOf<Pair<Int, Int>>()
-        if (x > 0) {
+        if (x > 0)
             indices.add(Pair(x - 1, y))
-        }
-        if (x < map.first().lastIndex) {
+        if (x < map.first().lastIndex)
             indices.add(Pair(x + 1, y))
-        }
-        if (y > 0) {
+        if (y > 0)
             indices.add(Pair(x, y - 1))
-        }
-        if (y < map.lastIndex) {
+        if (y < map.lastIndex)
             indices.add(Pair(x, y + 1))
-        }
         return indices
     }
 
@@ -70,7 +65,7 @@ fun main() {
         previous[v] = null
         queue.add(v to infinite)
     }
-    distances[source!!] = 0
+    distances[source] = 0
 
     while (queue.isNotEmpty()) {
         val (edge, _) = queue.poll()
@@ -88,7 +83,7 @@ fun main() {
     var cursor = destination
     val shortestPath = mutableListOf<EdgeId?>()
     do {
-        val step = previous.getValue(cursor!!) ?: continue
+        val step = previous.getValue(cursor) ?: continue
         shortestPath.add(step)
         cursor = step
 
