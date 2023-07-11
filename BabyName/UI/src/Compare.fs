77d040957dcc4ed2c957c663eb68837ebe011474
peheje <peterhj@gmail.com>
2023-07-11 17:24:19 +0200

    format and use string builder

--- BabyName/UI/src/Compare.fs
+++ BabyName/UI/src/Compare.fs
@@ -3,17 +3,24 @@ module Compare
 open Browser
 open Html
 open Browser.Types
+open System.Text
 
 let private compareData () =
     let readInput id =
         let ignoreCase = (inputFromId "case-insensitive").checked
-        let areaValue = if ignoreCase then (areaFromId id).value.ToLower()
-                        else (areaFromId id).value
-        let filtered = 
+
+        let areaValue =
+            if ignoreCase then
+                (areaFromId id).value.ToLower ()
+            else
+                (areaFromId id).value
+
+        let filtered =
             areaValue
             |> split newline
             |> Array.filter (fun x -> x.Trim() <> "")
             |> Array.distinct
+
         (filtered, filtered |> Set.ofArray)
 
     let aList, aSet = readInput "a"
@@ -34,35 +41,57 @@ let private compare () =
 let private random = System.Random()
 
 let private randomize () =
-    Array.init 10000 (fun _ -> random.Next(10000) |> string) |> setTextArea "a" "a-count"
-    Array.init 10000 (fun _ -> random.Next(10000) |> string) |> setTextArea "b" "b-count"
+    Array.init 10000 (fun _ -> random.Next(10000) |> string)
+    |> setTextArea "a" "a-count"
+
+    Array.init 10000 (fun _ -> random.Next(10000) |> string)
+    |> setTextArea "b" "b-count"
+
     compare ()
 
 let private download (event: Event) =
     let getValidSeparator () =
         let source = (areaFromId "a").value + (areaFromId "b").value
-        [|"|"; ";"; ","|] |> Array.tryFind (fun separator -> source |> contains separator |> not)
+
+        [| "|"; ";"; "," |]
+        |> Array.tryFind (fun separator -> source |> contains separator |> not)
 
     let takeOrEmpty source index =
-        match source |> Array.tryItem index with | None -> "" | Some v -> v
+        match source |> Array.tryItem index with
+        | None -> ""
+        | Some v -> v
 
     match getValidSeparator () with
-        | None ->
-            event.preventDefault()
-            window.alert "Download failed. Input already includes separator values | ; ,"
-        | Some separator ->
-            let (a, b, both, onlyA, onlyB) = compareData ()
-            let compareData = [|a; b; both; onlyA; onlyB|]
-            
-            let size = max (a |> Array.length) (b |> Array.length)
-            let mutable data = "Left" + separator + "Right" + separator + "In both" + separator + "Only in left" + separator + "Only in right\n"
-            for i in 0..size - 1 do
-                for item in compareData do
-                    data <- data + takeOrEmpty item i + separator
-                data <- data + "\n"
-            
-            let downloadBtn = (fromId "download-btn") :?> HTMLLinkElement
-            downloadBtn.href <- "data:text/plain;charset=UTF-8," + window.encodeURIComponent(data)
+    | None ->
+        event.preventDefault ()
+        window.alert "Download failed. Input already includes separator values | ; ,"
+    | Some separator ->
+        let (a, b, both, onlyA, onlyB) = compareData ()
+        let compareData = [| a; b; both; onlyA; onlyB |]
+
+        let size = max (a |> Array.length) (b |> Array.length)
+
+        let data =
+            StringBuilder(
+                "Left"
+                + separator
+                + "Right"
+                + separator
+                + "In both"
+                + separator
+                + "Only in left"
+                + separator
+                + "Only in right\n"
+            )
+
+        for i in 0 .. size - 1 do
+            for item in compareData do
+                data.Append(takeOrEmpty item i + separator) |> ignore
+
+            data.AppendLine() |> ignore
+
+        let downloadBtn = (fromId "download-btn") :?> HTMLLinkElement
+        downloadBtn.href <- "data:text/plain;charset=UTF-8," + window.encodeURIComponent (data.ToString())
 
 let initCompare () =
     fromId "compare-btn" |> onClick compare
@@ -71,6 +100,5 @@ let initCompare () =
 
 let handleCompareRedirect () =
     if window.location.pathname = "/compare/compare.html" then
-        window.setTimeout (fun _ ->
-            window.location.pathname <- "/compare.html"
-        , 6000) |> ignore
+        window.setTimeout ((fun _ -> window.location.pathname <- "/compare.html"), 6000)
+        |> ignore
