c327ff32d6518908cf941e5fd6020ddf7bf2eb6a
PEHEJE <peterhj@gmail.com>
2023-07-19 21:29:55 +0200

    use state

--- BabyName/UI/src/Memory.fs
+++ BabyName/UI/src/Memory.fs
@@ -13,17 +13,19 @@ let private hide id =
     element.classList.remove "inline-block"
     element.classList.add "display-none"
 
+type private State = Peek | Guess | Stopped
+
 let initMemory () =
 
     let random = System.Random()
     let submitBtn = inputFromId "submit"
     let ioInput = inputFromId "io"
     let intervalInput = inputFromId "interval"
-    let mutable state = "stopped"
+    let mutable state = Stopped
     let mutable number = ""
 
     let peek () =
-        state <- "peek"
+        state <- Peek
         show "io-input"
         hide "length-input"
         hide "interval-input"
@@ -32,20 +34,20 @@ let initMemory () =
         submitBtn.innerText <- "Peeking.."
     
     let stopPeek () =
-        state <- "guess"
+        state <- Guess
         ioInput.readOnly <- false
         submitBtn.disabled <- false
         submitBtn.innerText <- "Guess"
 
     submitBtn.addEventListener("click", (fun _ ->
         show "restart"
-        if state = "guess" then
+        if state = Guess then
             if ioInput.value = number then
                 window.alert "Correct"
             else
                 window.alert ("Not correct, number was: " + number)
-            state <- "stopped"
-        if state = "stopped" then
+            state <- Stopped
+        if state = Stopped then
             peek ()
             let length = (inputFromId "length").valueAsNumber |> int
             let randomNumbers = Array.init length (fun _ -> random.Next 10 |> string) |> join ""
@@ -59,12 +61,12 @@ let initMemory () =
             ), (intervalInput.valueAsNumber * 1000.0) |> int) |> ignore
     ))
 
-    (fromId "restart").addEventListener("click", (fun _ ->
+    (fromId "restart") |> onClick (fun _ ->
         window.location.reload ()
-    ))
+    )
 
     document.addEventListener("keypress", (fun e -> 
         let keyboardEvent = e :?> Types.KeyboardEvent
-        if keyboardEvent.key = "Enter" && state = "guess" && ioInput.value.Length <> 0 then
+        if keyboardEvent.key = "Enter" && state = Guess && ioInput.value.Length <> 0 then
             submitBtn.click ()
     ))
