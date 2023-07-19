3ffbe32f9a8178736ba6afec1e5c46b41f22c204
PEHEJE <peterhj@gmail.com>
2023-07-19 21:11:30 +0200

    unique

--- AOC2022/kotlin/aoc2022/src/main/kotlin/Main.kt
+++ AOC2022/kotlin/aoc2022/src/main/kotlin/Main.kt
@@ -1,106 +1,26 @@
 import java.io.File
-import java.util.PriorityQueue
-import kotlin.system.measureTimeMillis
+import java.nio.file.Path
+import kotlin.io.path.readLines
+import kotlin.time.ExperimentalTime
+import kotlin.time.TimeSource
 
-data class EdgeId(val v: Pair<Int, Int>)
-
+@OptIn(ExperimentalTime::class)
 fun main() {
-    measureTimeMillis {
-        aoc11()
-    }.let { println("${it}ms") }
-}
 
-private fun aoc12() {
-    val path = "/Users/phj/Code/F-Sharp-Advent-of-Code-2021/AOC2022/kotlin/aoc2022/src/main/kotlin/input12.txt"
-    val rows = File(path).readLines()
 
-    lateinit var destination: EdgeId
-    lateinit var source: EdgeId
+    val timeSource = TimeSource.Monotonic
+    val mark1 = timeSource.markNow()
 
-    val map = rows.mapIndexed { y, row ->
-        row.toList().mapIndexed { x, char ->
-            when (char) {
-                'S' -> {
-                    source = EdgeId(Pair(x, y))
-                    'a'.code
-                }
+    val path = "C:\\Users\\peter\\Repos\\LearningFSharp\\AOC2022\\kotlin\\aoc2022\\src\\main\\kotlin\\input10.txt"
+    val lines = kotlin.io.path.Path(path).readLines()
+    //val lines = File(path).readLines()
 
-                'E' -> {
-                    destination = EdgeId(Pair(x, y))
-                    'z'.code
-                }
+    val mark2 = timeSource.markNow()
 
-                else -> char.code
-            } - 97
-        }
-    }
+    val elapsed = mark2 - mark1
 
-    fun neighborIndices(x: Int, y: Int): List<EdgeId> {
-        val indices = mutableListOf<EdgeId>()
-        if (x > 0)
-            indices.add(EdgeId(Pair(x - 1, y)))
-        if (x < map.first().lastIndex)
-            indices.add(EdgeId(Pair(x + 1, y)))
-        if (y > 0)
-            indices.add(EdgeId(Pair(x, y - 1)))
-        if (y < map.lastIndex)
-            indices.add(EdgeId(Pair(x, y + 1)))
-        return indices
-    }
+    println("${elapsed.inWholeMilliseconds}ms")
 
-    val infinite = 1000
-
-    val graph = HashMap<EdgeId, List<Pair<EdgeId, Int>>>()
-    map.forEachIndexed { y, row ->
-        row.forEachIndexed { x, value ->
-            val indices = neighborIndices(x, y).map { neighbor ->
-                val (nx, ny) = neighbor.v
-                val difference = map[ny][nx] - value
-                val cost = if (difference > 1) infinite else 1
-                Pair(neighbor, cost)
-            }
-            graph[EdgeId(Pair(x, y))] = indices
-        }
-    }
-
-    val distances = hashMapOf<EdgeId, Int>()
-    val previous = HashMap<EdgeId, EdgeId?>()
-    val queue = PriorityQueue<Pair<EdgeId, Int>>(compareBy { it.second })
-
-    for (v in graph.keys) {
-        distances[v] = infinite
-        previous[v] = null
-        queue.add(v to infinite)
-    }
-    distances[source] = 0
-
-    while (queue.isNotEmpty()) {
-        val (edge, _) = queue.poll()
-
-        graph.getValue(edge).forEach { (neighbor, weight) ->
-            val totalDistance = distances.getValue(edge) + weight
-            if (totalDistance < distances.getValue(neighbor)) {
-                distances[neighbor] = totalDistance
-                previous[neighbor] = edge
-                queue.add(neighbor to totalDistance)
-            }
-        }
-    }
-
-    var cursor = destination
-    val shortestPath = mutableListOf<EdgeId?>()
-    do {
-        val step = previous.getValue(cursor) ?: continue
-        shortestPath.add(step)
-        cursor = step
-
-    } while (cursor != source)
-
-    shortestPath.reversed().forEach {
-        println(it)
-    }
-
-    println("Done with shortest path ${shortestPath.size}")
 }
 
 
--- BabyName/UI/src/Unique.fs
+++ BabyName/UI/src/Unique.fs
@@ -3,23 +3,35 @@ module Unique
 open Html
 
 let private readInput id ignoreCase =
-    let areaValue = if ignoreCase then (areaFromId id).value.ToLower()
-                    else (areaFromId id).value
-    areaValue
-        |> split newline
-        |> Array.filter (fun x -> x.Trim() <> "")
+    let areaValue =
+        if ignoreCase then
+            (areaFromId id).value.ToLower ()
+        else
+            (areaFromId id).value
+
+    areaValue |> split newline |> Array.filter (fun x -> x.Trim() <> "")
 
 let private getDuplicates xs =
     let o = System.Collections.Generic.HashSet<string>()
 
-    xs |> Array.choose (fun x ->
+    xs
+    |> Array.choose (fun x ->
         if o.Contains x then
             o.Add x |> ignore
             Some x
         else
             o.Add x |> ignore
-            None
+            None)
+
+// Unsure if this functional one is better than the mutating one?
+let private getDuplicates1 xs =
+    xs
+    |> Array.countBy id
+    |> Array.filter (fun (_, count) -> count > 1)
+    |> Array.map (fun (x, count) ->
+        Array.init (count-1) (fun _ -> x)
     )
+    |> Array.collect id
 
 let private findDuplicates () =
     let ignoreCase = (inputFromId "case-insensitive").checked
