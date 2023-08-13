93de60fcd92cb36ad16fa1a39c793865f9063a7b
PEHEJE <peterhj@gmail.com>
2023-08-13 12:49:05 +0200

    kotlin version

--- AOC2022/kotlin/aoc2022/src/main/kotlin/Apc12/Aoc12.kt
+++ AOC2022/kotlin/aoc2022/src/main/kotlin/Apc12/Aoc12.kt
@@ -7,21 +7,25 @@ import kotlin.collections.HashMap
 data class EdgeId(val v: Pair<Int, Int>)
 
 fun aoc12() {
-    val filepath = "/Users/phj/Code/F-Sharp-Advent-of-Code-2021/AOC2022/12/input12.txt"
+    val filepath = "C:\\Users\\peter\\Repos\\LearningFSharp\\AOC2022\\12\\input12.txt"
     val rows = File(filepath).readLines()
 
-    lateinit var source: EdgeId
+    lateinit var origSource: EdgeId
+    lateinit var destination: EdgeId
 
     val map = rows.mapIndexed { y, row ->
         row.toList().mapIndexed { x, char ->
             when (char) {
                 'S' -> {
-                    source = EdgeId(Pair(x, y))
+                    origSource = EdgeId(Pair(x, y))
                     'a'.code
                 }
+
                 'E' -> {
+                    destination = EdgeId(Pair(x, y))
                     'z'.code
                 }
+
                 else -> char.code
             } - 97
         }
@@ -55,60 +59,64 @@ fun aoc12() {
         }
     }
 
-    val distances = hashMapOf<EdgeId, Int>()
-    val previous = HashMap<EdgeId, EdgeId?>()
-    val queue = PriorityQueue<Pair<EdgeId, Int>>(compareBy { it.second })
+    fun dijkstra(source: EdgeId): HashMap<EdgeId, EdgeId?> {
+        val distances = hashMapOf<EdgeId, Int>()
+        val previous = HashMap<EdgeId, EdgeId?>()
+        val queue = PriorityQueue<Pair<EdgeId, Int>>(compareBy { it.second })
 
-    for (v in graph.keys) {
-        distances[v] = infinite
-        previous[v] = null
-        queue.add(v to infinite)
-    }
-    distances[source] = 0
+        for (v in graph.keys) {
+            distances[v] = infinite
+            previous[v] = null
+            queue.add(v to infinite)
+        }
+        distances[source] = 0
 
-    while (queue.isNotEmpty()) {
-        val (edge, _) = queue.poll()
+        while (queue.isNotEmpty()) {
+            val (edge, _) = queue.poll()
 
-        graph.getValue(edge).forEach { (neighbor, weight) ->
-            val totalDistance = distances.getValue(edge) + weight
-            if (totalDistance < distances.getValue(neighbor)) {
-                distances[neighbor] = totalDistance
-                previous[neighbor] = edge
-                queue.add(neighbor to totalDistance)
+            graph.getValue(edge).forEach { (neighbor, weight) ->
+                val totalDistance = distances.getValue(edge) + weight
+                if (totalDistance < distances.getValue(neighbor)) {
+                    distances[neighbor] = totalDistance
+                    previous[neighbor] = edge
+                    queue.add(neighbor to totalDistance)
+                }
             }
         }
+
+        return previous
     }
 
-    fun shortestPath(destination: EdgeId): MutableList<EdgeId?> {
+
+    fun shortestPath(source: EdgeId): List<EdgeId>? {
+
+        val previous = dijkstra(source)
+
         var cursor = destination
-        val shortestPath = mutableListOf<EdgeId?>()
+        val shortestPath = mutableListOf<EdgeId>()
         do {
-            val step = previous.getValue(cursor) ?: continue
+            val step = previous.getValue(cursor) ?: return null
             shortestPath.add(step)
             cursor = step
 
         } while (cursor != source)
 
-        return shortestPath
+        return shortestPath.toList()
     }
 
     val destinations = mutableListOf<EdgeId>()
     for (i in rows.indices) {
         for (j in rows[i].indices) {
             if (rows[i][j] == 'a' || rows[i][j] == 'S')
-                destinations.add(EdgeId(Pair(i, j)))
+                destinations.add(EdgeId(Pair(j, i)))
         }
     }
 
-    val min = destinations.minOf { shortestPath(it).size }
-    println("min is $min")
+    println("part 1 shortest path: ${shortestPath(origSource)?.size}")
 
-    for (destination in destinations) {
-        val path = shortestPath(destination)
-        path.reversed().forEach {
-            println(it)
-        }
-        // println("Done with shortest path ${path.size}")
-    }
-
+    val shortestPathFromLowestPoint =
+        destinations
+            .mapNotNull { shortestPath(it) }
+            .minOf { it.size }
+    println("part 2 shortestPathFromLowestPoint $shortestPathFromLowestPoint")
 }
