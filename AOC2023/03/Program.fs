8e6e624edaa8a668c44097fa9c4d34fed4f83990
peheje <peterhj@gmail.com>
2023-12-03 20:45:26 +0100

    part 3 1

---
+++ AOC2023/03/Program.fs
@@ -0,0 +1,58 @@
+﻿let rows = System.IO.File.ReadAllLines "C:\Users\peter\Repos\LearningFSharp\AOC2023\03\data"
+
+type Value =
+    | PartNumber of int
+    | Symbol of char
+    | Empty
+
+type Cell = { x: int; y: int; value: Value }
+
+let log x = printfn "%A" x
+
+let map =
+    rows
+    |> Array.map (fun row -> row |> Seq.toArray |> Array.map (fun c -> c))
+
+let isSymbol symbol =
+    symbol |> System.Char.IsDigit |> not
+    && symbol <> '.'
+
+let maxX = map[0].Length - 1
+let maxY = map.Length - 1
+
+let hasNeighborSymbol x y =
+    (x > 0 && map[y][x - 1] |> isSymbol)
+    || (x < maxX && map[y][x + 1] |> isSymbol)
+    || (y > 0 && map[y - 1][x] |> isSymbol)
+    || (y < maxY && map[y + 1][x] |> isSymbol)
+    || (x > 0 && y > 0 && map[y - 1][x - 1] |> isSymbol)
+    || (y < maxY && x < maxX && map[y + 1][x + 1] |> isSymbol)
+    || (y < maxY && x > 0 && map[y + 1][x - 1] |> isSymbol)
+    || (y > 0 && x < maxX && map[y - 1][x + 1] |> isSymbol)
+
+let part1 =
+    seq {
+
+        for (y, row) in map |> Array.indexed do
+
+            let sb = System.Text.StringBuilder()
+            let mutable valid = false
+
+            for (x, c) in row |> Array.indexed do
+                if c |> System.Char.IsDigit then
+                    sb.Append c |> ignore
+
+                    if hasNeighborSymbol x y then
+                        valid <- true
+                else
+                    if sb.Length <> 0 && valid then
+                        yield (sb.ToString() |> int)
+
+                    valid <- false
+                    sb.Clear() |> ignore
+            
+            if sb.Length <> 0 && valid then
+                yield (sb.ToString() |> int)
+
+    }
+    |> Seq.sum
