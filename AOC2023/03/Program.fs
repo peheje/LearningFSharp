6a022b40e92b756e8b359144da50ca0f3662aaab
peheje <peterhj@gmail.com>
2023-12-04 19:12:55 +0100

    hmm a lot of mutation here, f# doesnt feel streamlined for it, here kotling shines

--- AOC2023/03/Program.fs
+++ AOC2023/03/Program.fs
@@ -1,10 +1,15 @@
-﻿let rows = System.IO.File.ReadAllLines "C:\Users\peter\Repos\LearningFSharp\AOC2023\03\data"
+﻿open System
+
+let rows =
+    System.IO.File.ReadAllLines "C:\Users\peter\Repos\LearningFSharp\AOC2023\03\data"
 
 let log x = printfn "%A" x
 
 let map =
     rows
     |> Array.map (fun row -> row |> Seq.toArray |> Array.map id)
+
+let isGear symbol = symbol = '*'
 
 let isSymbol symbol =
     symbol |> System.Char.IsDigit |> not
@@ -19,7 +24,9 @@ let hasNeighborSymbol x y =
     || (y > 0 && map[y - 1][x] |> isSymbol)
     || (y < maxY && map[y + 1][x] |> isSymbol)
     || (x > 0 && y > 0 && map[y - 1][x - 1] |> isSymbol)
-    || (y < maxY && x < maxX && map[y + 1][x + 1] |> isSymbol)
+    || (y < maxY
+        && x < maxX
+        && map[y + 1][x + 1] |> isSymbol)
     || (y < maxY && x > 0 && map[y + 1][x - 1] |> isSymbol)
     || (y > 0 && x < maxX && map[y - 1][x + 1] |> isSymbol)
 
@@ -43,9 +50,82 @@ let part1 =
 
                     valid <- false
                     sb.Clear() |> ignore
-            
+
             if sb.Length <> 0 && valid then
                 yield (sb.ToString() |> int)
 
     }
     |> Seq.sum
+
+// Part 2
+type Coordinate = { x: int; y: int }
+
+type Gear = { coordinate: Coordinate }
+
+type Number =
+    { value: int
+      coordinates: Coordinate seq }
+
+let gears =
+    seq {
+        for (y, row) in map |> Array.indexed do
+            for (x, c) in row |> Array.indexed do
+                if c = '*' then
+                    yield { coordinate = { x = x; y = y } }
+    }
+
+let numbers =
+    seq {
+
+        for (y, row) in map |> Array.indexed do
+
+            let sb = Text.StringBuilder()
+            let indices = Collections.Generic.List<Coordinate>()
+
+            for (x, c) in row |> Array.indexed do
+                if c |> System.Char.IsDigit then
+                    sb.Append c |> ignore
+                    indices.Add({ x = x; y = y })
+                else
+                    if sb.Length <> 0 then
+                        yield
+                            { value = sb.ToString() |> int
+                              coordinates = indices }
+
+                    indices.Clear()
+                    sb.Clear() |> ignore
+
+            if sb.Length <> 0 then
+                yield
+                    { value = sb.ToString() |> int
+                      coordinates = indices }
+
+    }
+
+let isNeighbor x1 y1 x2 y2 =
+    let xd = abs(x1-x2)
+    let yd = abs (y1-y2)
+    xd < 2 && yd < 2
+
+let mutable sum = 0
+for gear in gears do
+    let gCoord = gear.coordinate
+    
+    let mutable numbersCollected = Collections.Generic.List<int>()
+    let mutable countNextToGear = 0
+    for number in numbers do
+        
+        if number.coordinates |> Seq.exists (fun numberCoord ->
+            isNeighbor numberCoord.x numberCoord.y gCoord.x gCoord.y
+        ) then
+            countNextToGear <- countNextToGear + 1
+            numbersCollected.Add (number.value)
+    
+    if countNextToGear = 2 then
+        printfn "GEAR: %A N1 %A N2 %A" gCoord numbersCollected[0] numbersCollected[1]
+        let power = numbersCollected[0] * numbersCollected[1]
+        sum <- sum + power
+
+    numbersCollected.Clear()
+
+   
