1a5388b673d2e87274db775c8d4589cdcc709a2c
Peter H. Jensen <peterhj@gmail.com>
2023-12-08 23:19:05 +0100

    refactor

--- AOC2023/04/Program.fs
+++ AOC2023/04/Program.fs
@@ -1,5 +1,4 @@
-﻿
-module String =
+﻿module String =
     let trim (x: string) = x.Trim()
     let split (c: char) (x: string) = x.Split c
     let indexOf (s: string) (x: string) = x.IndexOf s
@@ -25,8 +24,9 @@ let rowToPoint row =
     let ours = parseNumbers split[1] |> Set.ofArray
 
     let intersection = winning |> Set.intersect ours
-    2.0**((intersection |> Set.count |> float) - 1.0) |> int
+    2.0 ** ((intersection |> Set.count |> float) - 1.0) |> int
 
-let rows = System.IO.File.ReadAllLines @"C:\Users\peter\repos\LearningFSharp\AOC2023\04\data"
+let rows =
+    System.IO.File.ReadAllLines @"C:\Users\peter\repos\LearningFSharp\AOC2023\04\data"
 
 rows |> Array.sumBy rowToPoint
