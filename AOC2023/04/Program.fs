705860bbf8902a9e0c1bb9bc1c8a554c6349ece9
Peter H. Jensen <peterhj@gmail.com>
2023-12-09 20:58:09 +0100

    part 2

--- AOC2023/04/Program.fs
+++ AOC2023/04/Program.fs
@@ -1,32 +1,52 @@
-﻿module String =
-    let trim (x: string) = x.Trim()
-    let split (c: char) (x: string) = x.Split c
-    let indexOf (s: string) (x: string) = x.IndexOf s
-    let substring (i: int) (x: string) = x.Substring i
+﻿let logs x =
+    printfn "%A" x
+    x
 
-let rowToPoint row =
-    let cardNumberIndex = row |> String.indexOf ": "
-    let numbers = row |> String.substring (cardNumberIndex + 2)
-    let split = numbers |> String.split '|'
+let rowToMatches (row: string) =
+    let cardNumberIndex = row.IndexOf(": ")
+    let numbers = row.Substring(cardNumberIndex + 2)
+    let split = numbers.Split('|')
 
     let notEmpty (x: string) =
         System.String.IsNullOrWhiteSpace x |> not
 
-    let parseNumbers numbers =
-        numbers
-        |> String.trim
-        |> String.split ' '
-        |> Array.map String.trim
+    let parseNumbers (numbers: string) =
+        numbers.Trim().Split(' ')
+        |> Array.map _.Trim()
         |> Array.filter notEmpty
         |> Array.map int
 
     let winning = parseNumbers split[0] |> Set.ofArray
     let ours = parseNumbers split[1] |> Set.ofArray
 
-    let intersection = winning |> Set.intersect ours
-    2.0 ** ((intersection |> Set.count |> float) - 1.0) |> int
+    winning |> Set.intersect ours |> Set.count
+
+let rowToPoint (row: string) =
+    2.0 ** ((row |> rowToMatches |> float) - 1.0) |> int
 
 let rows =
     System.IO.File.ReadAllLines @"C:\Users\peter\repos\LearningFSharp\AOC2023\04\data"
 
-rows |> Array.sumBy rowToPoint
+let part1 = rows |> Array.sumBy rowToPoint
+printfn "Part 1 %i" part1
+
+// part 2
+
+let rowToCard (row: string) =
+    let cardNumberIndex = row.IndexOf(": ")
+    let cardNumber = row.Substring(0, cardNumberIndex).Replace("Card ", "") |> int
+    (cardNumber, rowToMatches row)
+
+let cardsSeq = rows |> Seq.map rowToCard
+let cardsMap = cardsSeq |> Map.ofSeq
+
+let rec followCards cardNumber =
+    match cardsMap[cardNumber] with
+    | 0 -> 0
+    | v ->
+        v + ([cardNumber + 1 .. cardNumber + v] |> Seq.sumBy (fun wonCardNumber ->
+            followCards wonCardNumber
+        ))
+        
+let part2 = (cardsSeq |> Seq.map (fst >> followCards) |> Seq.sum) + (cardsSeq |> Seq.length)
+printfn "Part 2 %i" part2
