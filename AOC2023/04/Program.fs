3e683557c128f708ff0f4c8e425795fc667d1822
Peter H. Jensen <peterhj@gmail.com>
2023-12-16 10:47:35 +0100

    golf

--- AOC2023/04/Program.fs
+++ AOC2023/04/Program.fs
@@ -1,18 +1,16 @@
 ﻿let rowToMatches (row: string) =
-    let cardNumberIndex = row.IndexOf(": ")
-    let numbers = row.Substring(cardNumberIndex + 2)
-    let split = numbers.Split('|')
+    let numbers = row.Split(": ")[1]
 
     let parseNumbers (numbers: string) =
-        numbers.Trim().Split(' ')
-        |> Seq.map _.Trim()
+        numbers.Split(' ')
         |> Seq.filter (System.String.IsNullOrWhiteSpace >> not)
         |> Seq.map int
+        |> Set.ofSeq
 
-    let winning = parseNumbers split[0] |> Set.ofSeq
-    let ours = parseNumbers split[1] |> Set.ofSeq
-
-    winning |> Set.intersect ours |> Set.count
+    numbers.Split('|')
+    |> Array.map parseNumbers
+    |> Set.intersectMany
+    |> Set.count
 
 let rowToPoint (row: string) =
     2.0 ** ((row |> rowToMatches |> float) - 1.0) |> int
@@ -23,8 +21,7 @@ let part1 = rows |> Array.sumBy rowToPoint
 printfn "Part 1 %i" part1
 
 let rowToCard (row: string) =
-    let cardNumberIndex = row.IndexOf(": ")
-    let cardNumber = row.Substring(0, cardNumberIndex).Replace("Card ", "") |> int
+    let cardNumber = (row.Split(": ")[0]).Replace("Card ", "") |> int
     (cardNumber, rowToMatches row)
 
 let cardsSeq = rows |> Seq.map rowToCard
@@ -33,10 +30,7 @@ let cardsMap = cardsSeq |> Map.ofSeq
 let rec followCards cardNumber =
     match cardsMap[cardNumber] with
     | 0 -> 0
-    | won ->
-        won
-        + ([ cardNumber + 1 .. cardNumber + won ]
-           |> Seq.sumBy followCards)
+    | won -> won + ([ cardNumber + 1 .. cardNumber + won ] |> Seq.sumBy followCards)
 
 let part2 =
     (cardsSeq |> Seq.map (fst >> followCards) |> Seq.sum) + (cardsSeq |> Seq.length)
