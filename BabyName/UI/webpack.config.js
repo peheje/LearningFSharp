f798e65d230a7b9509333d487a0442ae2c8345b8
Peter H. Jensen <peterhj@gmail.com>
2024-01-25 21:59:17 +0100

    update tools

--- BabyName/UI/webpack.config.js
+++ BabyName/UI/webpack.config.js
@@ -2,6 +2,8 @@
 // For a more comprehensive configuration check:
 // https://github.com/fable-compiler/webpack-config-template
 
+// build for production: npx webpack --config webpack.config.js --mode production
+
 var path = require("path");
 
 module.exports = {
